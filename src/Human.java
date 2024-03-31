8e872973e64cf23d0c6fb227f5c1c727b645657b
Dmitriy Kochkin <korochyn@yandex.ru>
2024-03-31 10:24:42 +0500

    Task 1

--- src/Human.java
+++ src/Human.java
@@ -1,11 +1,97 @@
-import java.time.chrono.ChronoLocalDate;
+import java.time.LocalDate;
 import java.util.List;
 
 public class Human {
     String name;
-    ChronoLocalDate dob, dod;
+    LocalDate dob, dod;
     Gender gender;
     List<Human> children;
-    List<Human> parents;
+    String mother, father;
 
+    public Human(String name, LocalDate dob, LocalDate dod, Gender gender) {
+        this.name = name;
+        this.dob = dob;
+        this.dod = dod;
+        this.gender = gender;
+
+    }
+
+    public Human(String name, LocalDate dob, LocalDate dod, Gender gender, List<Human> children, String mother, String father) {
+        this.name = name;
+        this.dob = dob;
+        this.dod = dod;
+        this.gender = gender;
+        this.children = children;
+        this.mother = mother;
+        this.father = father;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public LocalDate getDod() {
+        return dod;
+    }
+
+    public void setDod(LocalDate dod) {
+        this.dod = dod;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Human> children) {
+        this.children = children;
+    }
+
+    public String getMother() {
+        return mother;
+    }
+
+    public void setMother(String mother) {
+        this.mother = mother;
+    }
+
+    public String getFather() {
+        return father;
+    }
+
+    public void setFather(String father) {
+        this.father = father;
+    }
+
+    @Override
+    public String toString() {
+        return "\nHuman{" +
+                "name='" + name + '\'' +
+                ", dob=" + dob +
+                ", dod=" + dod +
+                ", gender=" + gender +
+                ",\n children=" + children +
+               ", \n mather=" + mother +
+                ", father=" + father +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        Human human1 = new Human("Ivan Konstantinov", LocalDate.of(1999, Month.APRIL,12), null, Gender.Male );
+        Human human2 = new Human("Irina Konstantinova", LocalDate.of(1998, Month.DECEMBER,12), null, Gender.Female );
+        Human human3 = new Human("Maria Petrova", LocalDate.of(1964, Month.APRIL,12), null, Gender.Female );
+        Human human4 = new Human("Semen Konstantinov", LocalDate.of(1968, Month.DECEMBER,12), null, Gender.Male );
 
+        List<Human> children1 = new ArrayList<>();
+        children1.add(human1);
+        children1.add(human2);
+        human4.setChildren(children1);
+        human3.setChildren(children1);
+
+        human1.setMother(human3.getName());
+        human1.setFather(human4.getName());
+        human2.setMother(human3.getName());
+        human2.setFather(human4.getName());
+
+        FamilyTree familyTree1 = new FamilyTree();
+        familyTree1.addHuman(human1);
+        familyTree1.addHuman(human2);
+        familyTree1.addHuman(human3);
+        familyTree1.addHuman(human4);
+
+        System.out.println(familyTree1);
     }
 }
