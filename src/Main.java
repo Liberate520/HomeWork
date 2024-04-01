b2669fd2245362618895dfd8b705b9fff518bc06
Dilyara <flay.sunset@mail.ru>
2024-04-01 08:35:19 +0300

    Object oriented programming - Task 1 - Solution added

---
+++ src/Main.java
@@ -0,0 +1,124 @@
+import ru.gb.family_tree.family_tree.FamilyTree;
+import ru.gb.family_tree.human.Human;
+import ru.gb.family_tree.human.Gender;
+import ru.gb.family_tree.human.SpouseStatus;
+
+import java.time.LocalDate;
+
+
+public class Main {
+    public static void main(String[] args) {
+        FamilyTree familyTree = new FamilyTree();
+        Human human1 = new Human("Иванов Валерий Петрович", Gender.male, LocalDate.of(1978, 4, 11));
+        Human human2 = new Human("Максимов Петр Геннадьевич", Gender.male, LocalDate.of(1957, 12,4), LocalDate.of(1999, 6, 12));
+        Human human3 = new Human("Макарова Инна Сергеевна", Gender.female, LocalDate.of(1958, 9,4), LocalDate.of(2001, 9, 19));
+        Human human4 = new Human("Максимов Валерий Петрович", Gender.male, LocalDate.of(1988, 5, 7));
+        Human human5 = new Human("Максимова Анна Петровна", Gender.female, LocalDate.of(1991, 11, 13));
+        Human human6 = new Human("Родионова Анастасия Павловна", Gender.female, LocalDate.of(1960, 12, 1));
+
+
+        human1.PrintName();
+        human2.PrintName();
+
+        human3.PrintDob();
+        human4.PrintDob();
+
+        human3.PrintDod();
+        human4.PrintDod();
+
+        human1.PrintGender();
+        human3.PrintGender();
+
+        human1.PrintAge();
+        human2.PrintAge();
+
+        human2.PrintMother();
+        human3.PrintMother();
+
+        human2.PrintFather();
+        human3.PrintFather();
+
+        human4.PrintFather();
+        human4.setFather(human2);
+        human4.PrintFather();
+
+        human4.PrintMother();
+        human4.setMother(human3);
+        human4.PrintMother();
+
+        human5.PrintFather();
+        human5.setFather(human2);
+        human5.PrintFather();
+
+        human5.PrintMother();
+        human5.setMother(human3);
+        human5.PrintMother();
+
+        System.out.println("--------");
+
+        human2.PrintChildren();
+        human2.addChild(human4);
+        human2.PrintChildren();
+        human2.addChild(human5);
+        human2.PrintChildren();
+
+        human3.PrintChildren();
+        human3.addChild(human4);
+        human3.PrintChildren();
+        human3.addChild(human5);
+        human3.PrintChildren();
+
+        System.out.println("--------");
+
+        human2.PrintSpouses();
+        human2.addSpouse(human3, SpouseStatus.Ex);
+        human2.PrintSpouses();
+
+        human3.PrintSpouses();
+        human3.addSpouse(human2, SpouseStatus.Actual);
+        human3.PrintSpouses();
+
+        human2.PrintSpouses();
+        human2.addSpouse(human6, SpouseStatus.Actual);
+        human2.PrintSpouses();
+
+        System.out.println("--------");
+
+        System.out.println(human1);
+        System.out.println(human2);
+        System.out.println(human3);
+        System.out.println(human4);
+        System.out.println(human5);
+
+        System.out.println("--------");
+
+
+        familyTree.addHuman(human1);
+        familyTree.addHuman(human2);
+        familyTree.addHuman(human3);
+        familyTree.addHuman(human4);
+        familyTree.addHuman(human5);
+
+        human1.PrintId();
+
+        System.out.println("--------");
+
+
+        familyTree.findById(0);
+        familyTree.findById(3);
+
+        System.out.println("--------");
+
+
+        familyTree.findByName("Макарова Инна Сергеевна");
+
+
+        System.out.println("--------");
+
+        System.out.println(familyTree);
+
+    }
+}
+
+
+//---------------------------------------------------------------------------------------
