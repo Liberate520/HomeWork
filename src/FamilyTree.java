36b99fcedcb42966d2f34ae34cb84abf1bfa407b
alexd <1090424@gmail.com>
2024-04-06 03:14:09 +0300

    third commit

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class FamilyTree {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Human> humanList;
+
+    public FamilyTree(){
+        humanList = new ArrayList<>();
+    }
+
+    public boolean addHumantoTree (Human human) {
+        if (!humanList.contains(human)){
+            humanList.add(human);
+            addhumantoParents(human);
+            addhumantoChild(human);
+        }
+        return true;
+    }
+
+    private boolean addhumantoChild(Human human) {
+        for(Human child: human.getChildrenList()){
+            child.addParents(human);
+        }
+        return true;
+    }
+
+    private void addhumantoParents(Human human) {
+        for(Human parent: human.getParents()){
+            parent.addChildren(human);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+    public String getInfo(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("В семейном дереве сейчас: ");
+        sb.append(humanList.size()).append(" Чел."+"\n");
+        for (Human human: humanList) {
+            sb.append(human);
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
+
+
--- src/NumerateHuman.java
+++ src/NumerateHuman.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class NumerateHuman {
+public class NumerateHuman {
+    private int id;
+    private static int humancount = 0;
+
+    public NumerateHuman(){
+        humancount++;
+        id = humancount;
+    }
+    public int gethumanId() {
+        return this.id;
+
+    }
 }
