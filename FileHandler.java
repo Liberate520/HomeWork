2d185130584c8e9f7e67971eb04c2c7dc63d5533
NoMaksich <maxummetalnikov@mail.ru>
2024-04-09 21:22:03 +0300

    был создан класс для сохранения/чтения из файла

---
+++ FileHandler.java
@@ -0,0 +1,26 @@
+package homeWork;
+
+import java.io.*;
+
+public class FileHandler implements Writable {
+    public void writeFile(Serializable serializable, String filename) {
+        try(ObjectOutputStream objectOutputStream  = new ObjectOutputStream(new FileOutputStream(filename))){
+            objectOutputStream .writeObject(serializable);
+            objectOutputStream.close();
+        }
+        catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public Object readFile(String filename) {
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filename))) {
+            Object list = objectInputStream.readObject();
+            objectInputStream.close();
+            return list;
+        } catch (Exception e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+}
