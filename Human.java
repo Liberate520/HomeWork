38c2f1fc86430adeb58e6bc4ca3352b952d1aad6
NoMaksich <maxummetalnikov@mail.ru>
2024-04-09 21:21:05 +0300

    был изменен файл FamilyTree.java

--- Human.java
+++ Human.java
@@ -1,22 +1,19 @@
 package homeWork;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Human {
+public class Human implements Serializable{
     String name;
     LocalDate dob, dod;
     Gender gender;
     List<Human> children = new ArrayList<>();
     Human mother, father;
-    int age;
     
     public Human(String name, LocalDate dob, Gender gender){
-        this.name=name;
-        this.dob=dob;
-        this.gender=gender;
-        this.age=this.GetAge();
+        this(name, dob, null, gender);
     }
 
     public Human(String name, LocalDate dob, LocalDate dod, Gender gender){
@@ -24,41 +21,40 @@ public class Human {
         this.dob=dob;
         this.dod=dod;
         this.gender=gender;
-        this.age=this.GetAge();
     }
 
-    public int GetAge(){
+    public int getAge(){
         LocalDate endDate = dod != null ? dod : LocalDate.now();
         int age = endDate.getYear() - dob.getYear();
 
         if (dob.getMonthValue() > endDate.getMonthValue() || (dob.getMonthValue() == endDate.getMonthValue() && dob.getDayOfMonth() > endDate.getDayOfMonth())) {
             age--;
         }
-        this.age=age;
         return age;
     }
     
-    public void setFamily(Human parent) {
-        if (parent == mother) {
-            this.mother = parent;
-        } else if (parent == father) {
-            this.father = parent;
-        } else {
-            children.add(parent);
+    public void addChild(Human child, Human mother, Human father) {
+        child.mother = mother;
+        child.father = father;
+
+        mother.children.add(child);
+        father.children.add(child);
+        
+        this.children.add(child);
+    }
+    
+    public List<Human> getChildren(Human parent) {
+        List<Human> childrenOfParent = new ArrayList<>();
+        for (Human child : this.children) {
+            if ((child.mother == parent || child.father == parent) && !childrenOfParent.contains(child)) {
+                childrenOfParent.add(child);
+            }
         }
-    }
-    
-    public void addChild(Human child) {
-        children.add(child);
-    }
-    
-    public List<Human> getChildren() {
-        System.out.println(this.name + "'s children(s): ");
-        return children;
+        return childrenOfParent;
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + ", age: " + this.age;
+        return "Name: " + name + ", age: " + Integer.toString(getAge()) + ", gender: " +  gender + ", mother: " + (mother != null ? mother.name : "Unknown") + ", father: " + (father != null ? father.name : "Unknown");
     }
 }
--- Main.java
+++ Main.java
@@ -5,25 +5,32 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         FamilyTree tree = new FamilyTree();
+
+        FileHandler fileHandler = new FileHandler();
+
         Human mother = new Human("Anna", LocalDate.of(1975, 5, 15), Gender.Female);
         Human father = new Human("Alexey", LocalDate.of(1970, 10, 25), Gender.Male);
-
         Human child1 = new Human("Goshka", LocalDate.of(2000, 3, 10), Gender.Female);
         Human child2 = new Human("Grishka", LocalDate.of(2005, 8, 20), Gender.Male);
+        Human child3 = new Human("Abc", LocalDate.of(2005, 8, 20), Gender.Male);
+        Human chel = new Human("MAxon", LocalDate.of(2005, 8, 20), Gender.Male);
 
-        child1.setFamily(mother);
-        child2.setFamily(father);
 
-        mother.addChild(child1);
-        father.addChild(child1);
-        father.addChild(child2);
+        mother.addChild(child1, mother, father);
+        mother.addChild(child2, mother, father);
+        child2.addChild(child3, child2, chel);
+
+
         tree.addTree(mother);
         tree.addTree(father);
         tree.addTree(child1);
         tree.addTree(child2);
+        tree.addTree(child3);
+        tree.addTree(chel);
 
-        System.out.println(mother.getChildren());
-        System.out.println(father.getChildren());
-        System.out.println(tree.printTree());
+
+        fileHandler.writeFile(tree, "familyTree.txt");
+        FamilyTree tree1 = (FamilyTree) fileHandler.readFile("familyTree.txt");
+        System.out.println(tree1.printTree());
     }
 }
