14737e53741f61adc8627a0a6fda200d8a47eaac
Damir Akhmetgaleev <artbox12345@mail.ru>
2024-04-14 21:29:06 +0500

    save66

--- src/family_tree/human/HumanComparatorByName.java
+++ src/family_tree/human/HumanComparatorByName.java
@@ -4,5 +4,7 @@ import java.util.Comparator;
 
 public class HumanComparatorByName  implements Comparator<Human> {
 
-public int compare(Human o1, Human o2) {return  o1.getName().compareTo(o2.getName());}
+    public int compare(Human o1, Human o2) {
+        return  o1.getName().compareTo(o2.getName());
+    }
 }
