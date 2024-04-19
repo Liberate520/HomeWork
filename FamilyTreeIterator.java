2ca44a01d677e0206b50e8d675810e0d235e44e7
Dmitry Poznyak <dimapoznyak1227@gmail.com>
2024-04-19 22:42:46 +0300

    qwert

---
+++ FamilyTreeIterator.java
@@ -0,0 +1,28 @@
+package homeWork;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class FamilyTreeIterator implements Iterator<Human> {
+    private List<Human> list;
+    private int index;
+
+    public FamilyTreeIterator(List<Human> list) {
+        this.list = list;
+        index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < list.size();
+    }
+
+    @Override
+    public Human next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return list.get(index++);
+    }
+}
