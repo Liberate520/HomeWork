926f17ac21e8871652969767c173f8e7d78415b2
IIIyt286 <63104032+IIIyt286@users.noreply.github.com>
2024-04-29 15:29:42 +0300

    Delete src/family_tree/io directory
    delet

--- src/family_tree/io/FileIO.java
+++
@@ -1,7 +0,0 @@
-package family_tree.io;
-import java.util.List;
-
-public interface FileIO {
-    void writeToFile(String filename, List<String> data);
-    List<String> readFromFile(String filename);
-}
