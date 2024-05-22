603f56881aca0b51f4314dd2ddbb39ca79faf378
IgorVremea <48389129+IgorVremea@users.noreply.github.com>
2024-05-22 09:29:55 +0300

    Решение ошибки с инкапсуляцией в классе FamilyTree

--- src/familyTree/tree/FamilyTree.java
+++ src/familyTree/tree/FamilyTree.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FamilyTree {
-    List<Person> tree;
+    private List<Person> tree;
 
     public FamilyTree() {
         this.tree = new ArrayList<>();
