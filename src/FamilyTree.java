59b73fd3748c72fbe23a458ddbd2e653256cb5aa
Aleksandr Maramzin <maramzinaleksandr@gmail.com>
2024-05-22 22:46:04 +0500

    Complete Homework 1

---
+++ src/FamilyTree.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private long humansId = 1;
+    private final List<Human> humanList;
+
+    public FamilyTree(){
+        this(new ArrayList<>());
+    }
+
+    public FamilyTree(List<Human> humanList){
+        this.humanList = humanList;
+    }
+
+    public boolean add(Human human){
+        if(human == null){
+            return false;
+        }
+        if(!humanList.contains(human)){
+            humanList.add(human);
+            human.setId(humansId++);
+
+            addParents(human);
+            addChild(human);
+
+            return true;
+        }
+        return false;
+    }
+
+    private void addParents(Human human){
+        if(human.getParents() != null) {
+            for (Human parent : human.getParents()) {
+                parent.addChild(human);
+            }
+        }
+    }
+
+    private void addChild(Human human){
+        if(human.getChildren() != null) {
+            for (Human child : human.getChildren()) {
+                child.addParents(human);
+            }
+        }
+    }
+
+    public List<Human> searchByName(String name){
+        List<Human> answer = new ArrayList<>();
+        for (Human human : humanList){
+            if(human.getName().equals(name)){
+                answer.add(human);
+            }
+        }
+        return answer;
+    }
+
+    public boolean setWadding(long humansId1, long humansId2){
+        if(checkId(humansId1) && checkId(humansId2)){
+            Human human1 = searchById(humansId1);
+            Human human2 = searchById(humansId2);
+            if(human1.getSpouse() == null && human2.getSpouse() == null){
+                human1.setSpouse(human2);
+                human2.setSpouse(human1);
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkId(long id){
+        return id < humansId && id > 0;
+    }
+
+    public Human searchById(long id){
+        for (Human human : humanList){
+            if(human.getId() == id){
+                return human;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human1 = new Human("Василий", Gender.Male, LocalDate.of(2001, 10, 2), null, null, null, null, null);
+        Human human2 = new Human("Светлана", Gender.Female, LocalDate.of(2002, 11, 3), null, null, null, null, null);
+
+        FamilyTree familyTree = new FamilyTree();
+
+        familyTree.add(human1);
+        familyTree.add(human2);
+        familyTree.setWadding(human1.getId(), human2.getId());
+
+
+        Human human3 = new Human("Виталий", Gender.Male, LocalDate.of(2022, 10, 6), null, human1, human2, null, null);
+
+        familyTree.add(human3);
+
+        System.out.println(human3);
+    }
+}
