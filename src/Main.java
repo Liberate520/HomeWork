1720dfb7c8a8d854c105178077d23708acbf6107
wsvetkin <wsvetkin@gmail.com>
2024-05-22 20:22:57 +0300

    create classes: FamilyTree

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,11 @@ import ru.gb.family_tree.Gender;
 import ru.gb.family_tree.Human;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Human human1 = new Human("Иван", "Гагарин", Gender.мужской,
-                LocalDate.ofEpochDay(1900 - 01 - 01), LocalDate.ofEpochDay(1978 - 03 - 17),
-                (Human) null, null, null, null); // начало рода прадед
-        Human human2 = new Human("Софья", "Яшина", Gender.женский,
-                LocalDate.ofEpochDay(1903 - 05 - 02), LocalDate.ofEpochDay(1980 - 01 - 19),
-                null, null, null, human1);
 
-        System.out.println(human1);
-        System.out.println(human2);
 
     }
 }
--- src/ru/gb/family_tree/Human.java
+++ src/ru/gb/family_tree/Human.java
@@ -1,77 +1,171 @@
 package ru.gb.family_tree;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Human {
+    private long id;
     private String name;
     private String lastName;
     private Gender gender;
     private LocalDate dateOfBirth;
     private LocalDate dateOfDeath;
-    private Human father;
-    private Human mather;
-    private List<Human> child;
+    private List<Human> parents;
+    private List<Human> children;
     private Human marriedPartner;
 
-    public Human(String name, String lastName, Gender gender, LocalDate dateOfBirth, LocalDate dateOfDeath, Human father, Human mather, List<Human> child, Human marriedPartner) {
+    public Human(String name, String lastName, Gender gender, LocalDate dateOfBirth,
+                 LocalDate dateOfDeath, Human father, Human mather) {
+        id = -1;
         this.name = name;
         this.lastName = lastName;
         this.gender = gender;
         this.dateOfBirth = dateOfBirth;
         this.dateOfDeath = dateOfDeath;
-        this.father = father;
-        this.mather = mather;
-        this.child = child;
-        this.marriedPartner = marriedPartner;
+        parents = new ArrayList<>();
+        if (father != null) {
+            parents.add(father);
+        }
+        if (mather != null) {
+            parents.add(mather);
+        }
+        children = new ArrayList<>();
+
     }
 
-    public Human(String иван, String гагарин) {
+    public Human(String name, String lastName, Gender gender, LocalDate dateOfBirth) {
+        this(name, lastName, gender, dateOfBirth, null, null, null);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public Human(String name, String lastName, Gender gender, LocalDate dateOfBirth,
+                 Human father, Human mather) {
+        this(name, lastName, gender, dateOfBirth, null, father, mather);
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public boolean addChild(Human child) {
+        if (!children.contains(child)) {
+            children.add(child);
+            return true;
+        }
+        return false;
     }
 
-    public void setGender(Gender gender) {
-        this.gender = gender;
+    public boolean addParent(Human parent) {
+        if (!children.contains(parent)) {
+            children.add(parent);
+            return true;
+        }
+        return false;
     }
 
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public Human getFather() {
+        for (Human parent : parents) {
+            if (parent.getGender() == Gender.мужской) {
+                return parent;
+            }
+        }
+        return null;
     }
 
-    public void setDateOfDeath(LocalDate dateOfDeath) {
-        this.dateOfDeath = dateOfDeath;
+    public Human getMather() {
+        for (Human parent : parents) {
+            if (parent.getGender() == Gender.женский) {
+                return parent;
+            }
+        }
+        return null;
     }
 
-    public void setFather(Human father) {
-        this.father = father;
+    public int getAge() {
+        if (dateOfDeath == null) {
+            return getPeriod(dateOfBirth, LocalDate.now());
+        } else {
+            return getPeriod(dateOfBirth, dateOfDeath);
+        }
     }
 
-    public void setMather(Human mather) {
-        this.mather = mather;
-    }
-
-    public void setChild(List<Human> child) {
-        this.child = child;
+    private int getPeriod(LocalDate dateOfBirth, LocalDate dateOfDeath) {
+        Period diff = Period.between(dateOfBirth, dateOfDeath);
+        return diff.getYears();
     }
 
     public void setMarriedPartner(Human marriedPartner) {
         this.marriedPartner = marriedPartner;
     }
 
+    public Human getMarriedPartner() {
+        return marriedPartner;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getLastName() {
         return lastName;
     }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(LocalDate dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public List<Human> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Human> parents) {
+        this.parents = parents;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Human> children) {
+        this.children = children;
+    }
+
+    //    public Human(String иван, String гагарин) {
+//    }
 
     @Override
     public String toString() {
@@ -81,10 +175,24 @@ public class Human {
                 ", пол: " + gender +
                 ", годы жизни: " + dateOfBirth +
                 " - " + dateOfDeath +
-                ", отец: " + father +
-                ", мать: " + mather +
-                ", дети: " + child +
+                ", родители: " + parents +
+                ", дети: " + children +
                 ", супруг: " + marriedPartner +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Human)) {
+            return false;
+        }
+        Human human = (Human) obj;
+        return human.getId() == getId();
+    }
+
 }
+
+
