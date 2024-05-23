8ac90c771d7a3fdc4f08478ee61201dc439873d3
NukeTree3 <nuketree3@gmail.com>
2024-05-23 19:54:47 +0300

    Создание коммита и классов

---
+++ src/ru/gb/famaly_tree/Famaly_tree.java
@@ -0,0 +1,4 @@
+package ru.gb.famaly_tree;
+
+public class Famaly_tree {
+}
---
+++ src/ru/gb/famaly_tree/Human.java
@@ -0,0 +1,15 @@
+package ru.gb.famaly_tree;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Human {
+        private String name;
+        private LocalDate birthDate, deathDate;
+        // private boolean sex;
+        Human mother, father;
+        List<Human> children;
+        Gender gender;
+// добавить метод возрата возраста
+
+}
