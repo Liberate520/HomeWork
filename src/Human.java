60ef9d6c1b7c19f6d42701939095cd64e03fa376
Viktor Golev <vwpiligrim@gmail.com>
2024-05-25 13:48:32 +0300

    rewrote the Human class logic in the Family_Tree_Vesion_Two branch

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,3 @@
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,50 +5,86 @@ import java.util.List;
  * Класс для представления человека в генеалогическом древе.
  * Содержит информацию о человеке, его родителях и детях.
  */
-public class Human implements Serializable {
-    private String name;
-    private Gender gender;
-    private List<Human> parents;
-    private List<Human> children;
-    private Human spouse;
+public class Human {
+    private String fullName; // Полное имя человека
+    private Gender gender; // Пол человека
+    private String birthDate; // Дата рождения человека
+    private String deathDate; // Дата смерти человека
+    private Human mother; // Мать человека
+    private Human father; // Отец человека
+    private List<Human> children; // Дети человека
+    private Human spouse; // Супруг(а) человека
 
-    public Human(String name, Gender gender) {
-        this.name = name;
+    public Human(String fullName, Gender gender, String birthDate, String deathDate) {
+        this.fullName = fullName;
         this.gender = gender;
-        this.parents = new ArrayList<>();
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
         this.children = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
+    // Метод для установки матери человека
+    public void setMother(Human mother) {
+        this.mother = mother;
     }
 
-    public Gender getGender() {
-        return gender;
+    // Метод для установки отца человека
+    public void setFather(Human father) {
+        this.father = father;
     }
 
-    public List<Human> getParents() {
-        return parents;
-    }
-
-    public List<Human> getChildren() {
-        return children;
-    }
-
-    public Human getSpouse() {
-        return spouse;
-    }
-
-    public void setParents(Human parent1, Human parent2) {
-        parents.add(parent1);
-        parents.add(parent2);
-    }
-
+    // Метод для добавления ребенка человеку
     public void addChild(Human child) {
         children.add(child);
     }
 
+    // Метод для установки супруга(и) человека
     public void setSpouse(Human spouse) {
         this.spouse = spouse;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Имя: ").append(fullName).append("\n");
+        stringBuilder.append("Пол: ").append(gender).append("\n");
+        stringBuilder.append("Дата рождения: ").append(birthDate).append("\n");
+        stringBuilder.append("Дата смерти: ").append(deathDate).append("\n");
+
+        if (mother != null) {
+            stringBuilder.append("Мать: ").append(mother.fullName).append("\n");
+        }
+
+        if (father != null) {
+            stringBuilder.append("Отец: ").append(father.fullName).append("\n");
+        }
+
+        if (!children.isEmpty()) {
+            stringBuilder.append("Дети:\n");
+            for (Human child : children) {
+                stringBuilder.append("- ").append(child.fullName).append("\n");
+            }
+        }
+
+        if (spouse != null) {
+            stringBuilder.append("Супруг(а): ").append(spouse.fullName).append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public boolean hasChild(String personName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasChild'");
+    }
+
+    public Human getSpouse() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSpouse'");
+    }
+
+    public char[] getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
     }
 }
