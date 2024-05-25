5481ded95a1c59c624f04c07e016e0950224ffc6
Viktor Golev <vwpiligrim@gmail.com>
2024-05-25 13:56:12 +0300

    rewrote the RelationshipSearch class logic in the Family_Tree_Vesion_Two branch

--- src/RelationshipSearch.java
+++ src/RelationshipSearch.java
@@ -6,52 +6,76 @@ import java.util.List;
  * Предоставляет методы для поиска родителей, супруга, братьев/сестер выбранного человека.
  */
 public class RelationshipSearch {
-    /**
-     * Возвращает список родителей указанного человека.
-     *
-     * @param person человек, для которого нужно найти родителей
-     * @return список родителей
-     */
-    public List<Human> getParents(Human person) {
-        List<Human> parents = new ArrayList<>();
-        for (Human member : person.getParents()) {
-            parents.add(member);
+
+    private FamilyTree familyTree; // Генеалогическое древо
+
+    public RelationshipSearch(FamilyTree familyTree) {
+        this.familyTree = familyTree;
+    }
+
+    // Метод для поиска всех детей указанного человека
+    public List<Human> findChildren(String personName) {
+        List<Human> children = new ArrayList<>();
+
+        // Проходим по каждому члену семьи в генеалогическом древе
+        for (Human member : familyTree.getMembers()) {
+            // Проверяем, является ли данный член родителем
+            if (member.hasChild(personName)) {
+                // Если да, добавляем его в список детей
+                children.add(member);
+            }
         }
+
+        return children;
+    }
+
+    // Метод для поиска всех родителей указанного человека
+    public List<Human> findParents(String personName) {
+        List<Human> parents = new ArrayList<>();
+
+        // Проходим по каждому члену семьи в генеалогическом древе
+        for (Human member : familyTree.getMembers()) {
+            // Проверяем, является ли данный член ребенком
+            if (member.hasChild(personName)) {
+                // Если да, добавляем его родителей в список родителей
+                parents.add(member.getSpouse());
+                parents.add(member);
+            }
+        }
+
         return parents;
     }
 
-    /**
-     * Возвращает супруга указанного человека.
-     *
-     * @param person человек, для которого нужно найти супруга
-     * @return супруг человека
-     */
-    public Human getSpouse(Human person) {
-        return person.getSpouse();
-    }
+    public static void main(String[] args) {
+        // Создаем генеалогическое древо и добавляем членов семьи
+        FamilyTree familyTree = new FamilyTree();
+        Human father = new Human("Иван Петров", Gender.MALE, "01.01.1960", "");
+        Human mother = new Human("Мария Петрова", Gender.FEMALE, "01.01.1965", "");
+        Human child1 = new Human("Анна Петрова", Gender.FEMALE, "01.01.1990", "");
+        Human child2 = new Human("Павел Петров", Gender.MALE, "01.01.1995", "");
+        father.setSpouse(mother);
+        mother.setSpouse(father);
+        father.addChild(child1);
+        father.addChild(child2);
+        familyTree.addMember(father);
+        familyTree.addMember(mother);
+        familyTree.addMember(child1);
+        familyTree.addMember(child2);
 
-    /**
-     * Возвращает список братьев и сестер указанного человека.
-     *
-     * @param person человек, для которого нужно найти братьев и сестер
-     * @return список братьев и сестер
-     */
-    public List<Human> getSiblings(Human person) {
-        List<Human> siblings = new ArrayList<>();
-        if (person.getParents().size() == 2) {
-            Human parent1 = person.getParents().get(0);
-            Human parent2 = person.getParents().get(1);
-            for (Human child : parent1.getChildren()) {
-                if (!child.equals(person)) {
-                    siblings.add(child);
-                }
-            }
-            for (Human child : parent2.getChildren()) {
-                if (!child.equals(person) && !siblings.contains(child)) {
-                    siblings.add(child);
-                }
-            }
+        // Создаем объект RelationshipSearch и выполняем поиск детей и родителей
+        RelationshipSearch relationshipSearch = new RelationshipSearch(familyTree);
+        List<Human> children = relationshipSearch.findChildren("Иван Петров");
+        List<Human> parents = relationshipSearch.findParents("Анна Петрова");
+
+        // Выводим результаты поиска
+        System.out.println("Дети Ивана Петрова:");
+        for (Human child : children) {
+            System.out.println(child.getName());
         }
-        return siblings;
+
+        System.out.println("Родители Анны Петровой:");
+        for (Human parent : parents) {
+            System.out.println(parent.getName());
+        }
     }
 }
