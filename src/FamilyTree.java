474a288f57455fdeaddd692cecc027c0008d0da3
Anna Sapr <annsapronova5492818@mail.ru>
2024-05-29 22:06:12 +0300

    supplemented 2

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -2,16 +2,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 class FamilyTree {
-    private List<Human> people;
+    private List<Human> humanList;
+
     public FamilyTree() {
-        this.people = new ArrayList<>();
-    }
-    public void addHuman(Human human) {
-        this.people.add(human);
+        this.humanList = new ArrayList<>();
     }
 
-    public Human findHumanByName(String name) {
-        for (Human human : people) {
+    public FamilyTree(List<Human> humanList) {
+        this.humanList = humanList;
+    }
+
+    public boolean addHuman(Human human) {
+        if (human == null) {
+            return false;
+        }
+        if (!humanList.contains(human)){
+            humanList.add(human);
+
+            addToParents(human);
+            addToChildren(human);
+
+            return true;
+        }
+        return false;
+    }
+
+    private void addToParents(Human human) {
+        for (Human parent: human.getParents()) {
+            parent.addChild(human);
+        }
+    }
+
+    private void addToChildren(Human human) {
+        for (Human child: human.getChildren()) {
+            child.addParent(human);
+        }
+    }
+
+    public Human getHumanByName(String name) {
+        for (Human human : humanList) {
             if (human.getName().equals(name)) {
                 return human;
             }
--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args){
         FamilyTree familyTree = new FamilyTree();
 
-        Human john = new Human("John", LocalDate.of(1980, 5, 15), "male");
-        Human jane = new Human("Jane", LocalDate.of(1982, 7, 10), "female");
-        Human mary = new Human("Mary", LocalDate.of(2010, 3, 25), "female");
-        Human anna = new Human("Anna", LocalDate.of(1955, 2, 17), LocalDate.of(2020, 1, 20), "female");
+        Human john = new Human("John", LocalDate.of(1980, 5, 15), Gender.Male);
+        Human jane = new Human("Jane", LocalDate.of(1982, 7, 10), Gender.Female);
+        Human mary = new Human("Mary", LocalDate.of(2010, 3, 25), Gender.Female);
+        Human anna = new Human("Anna", LocalDate.of(1955, 2, 17), LocalDate.of(2020, 1, 20), Gender.Female);
 
         john.addChild(mary);
         jane.addChild(mary);
@@ -20,7 +20,7 @@ public class Main {
         familyTree.addHuman(anna);
 
         // Проводим исследование: находим детей Anna
-        Human targetHuman = familyTree.findHumanByName("Anna");
+        Human targetHuman = familyTree.getHumanByName("Anna");
         if (targetHuman != null) {
             List<Human> children = familyTree.getChildrenOf(targetHuman);
             System.out.println("Дети " + targetHuman.getName() + ": " + children);
