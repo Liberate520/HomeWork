1673a028eea1ae85ca44f30fa1168ba15c3ec111
Dimase <zwere4ek@list.ru>
2024-05-31 08:11:36 +0300

    add Test.java code ^_^

---
+++ src/FamilyTree.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Person> members;
+
+    public FamilyTree() {
+        this.members = new ArrayList<>();
+    }
+
+    public void addMember(Person person) {
+        this.members.add(person);
+    }
+
+    public List<Person> getChildrenOf(Person parent) {
+        return parent.getChildren();
+    }
+
+    public Person findPersonByName(String name) {
+        for (Person person : members) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,33 @@
 public class Test {
+    public static void test(String[] args) {
+        // Создание экземпляра генеалогического древа
+        FamilyTree familyTree = new FamilyTree();
+
+        // Создание людей
+        Person john = new Person("John", 50);
+        Person jane = new Person("Jane", 48);
+        Person mike = new Person("Mike", 20);
+        Person anna = new Person("Anna", 18);
+
+        // Добавление детей
+        john.addChild(mike);
+        john.addChild(anna);
+
+        // Добавление людей в генеалогическое древо
+        familyTree.addMember(john);
+        familyTree.addMember(jane);
+        familyTree.addMember(mike);
+        familyTree.addMember(anna);
+
+        // Проведение исследования: получение всех детей Джона
+        Person parent = familyTree.findPersonByName("John");
+        if (parent != null) {
+            System.out.println("Children of " + parent.getName() + ":");
+            for (Person child : familyTree.getChildrenOf(parent)) {
+                System.out.println(child);
+            }
+        } else {
+            System.out.println("Person not found");
+        }
+    }
 }
