606d1499ca7c6930c1a753d51abf18631f60cde1
Diman4a12 <Diman4a65@gmail.com>
2024-06-05 22:35:46 +0500

    add class addhuman

--- src/ru/gb/family_tree/view/ConsoleUI.java
+++ src/ru/gb/family_tree/view/ConsoleUI.java
@@ -1,6 +1,18 @@
 package ru.gb.family_tree.view;
 
+import ru.gb.family_tree.model.human.Gender;
+import ru.gb.family_tree.presenter.Presenter;
+
+import java.util.Scanner;
+
 public class ConsoleUI implements View{
+
+    private static final String INPUT_ERROR = "Вы ввели неверное значение";
+    private Scanner scanner;
+    private Presenter presenter;
+    private boolean work;
+    private MainMenu menu;
+
 
     @Override
     public void printAnswer(String text){
@@ -10,5 +22,19 @@ public class ConsoleUI implements View{
     @Override
     public void start(){
 
+    }
+
+    public void addHuman(){
+        System.out.println("Введите имя:");
+        String name = scanner.nextLine();
+        System.out.println("Введите дату рождения в формате ДД.ММ.ГГГГ:");
+        String birth = scanner.nextLine();
+        System.out.println("Введите пол в формате М/Ж:");
+        String genderString = scanner.nextLine();
+        Gender gender = Gender.Female;
+        if(genderString.equals("М")){
+            gender = Gender.Male;
+        }
+        presenter.addHuman(name, birth, gender);
     }
 }
