06b66ddc9d801fdf580e6bd59c25501ee64b2cc0
AnatoliyIB <153524711+AnatoliyIB@users.noreply.github.com>
2024-06-05 22:34:50 +0300

    hw3 FamilyTreeFileManager.java

--- FamilyTreeFileManager.java
+++ FamilyTreeFileManager.java
@@ -1,42 +1,144 @@
-package homeWork.FamilyTree;
+package homeWork.Person;
 
-import java.io.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
-import homeWork.Person.Person;
-import homeWork.Service.FileManager;
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-public class FamilyTreeFileManager implements FileManager {
+    private static final AtomicLong idGenerator = new AtomicLong(0);
+    private final Long id;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private Gender gender;
+    private Date birthDate;
+    private Date deathDate;
+    private List<Person> parents;
+    private List<Person> children;
+    private Person spouse;
 
-    // Метод для сохранения объекта FamilyTree в файл
-    @Override
-    public void saveFamilyTreeToFile(FamilyTree familyTree, File file) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-            oos.writeObject(familyTree);
+    public Person(String lastName, String firstName, String middleName, Date birthDate) {
+        this(lastName, firstName, middleName, null, birthDate, null, null, null, null);
+    }
+
+    public Person(String lastName, String firstName, String middleName, Gender gender, Date birthDate, 
+                  Date deathDate, List<Person> parents, List<Person> children, Person spouse) {
+        this.id = idGenerator.incrementAndGet();
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+        this.parents = parents != null ? new ArrayList<>(parents) : new ArrayList<>();
+        this.children = children != null ? new ArrayList<>(children) : new ArrayList<>();
+        this.spouse = spouse;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getDeathDate() {
+        return deathDate;
+    }
+
+    public void setDeathDate(Date deathDate) {
+        this.deathDate = deathDate;
+    }
+
+    public List<Person> getParents() {
+        return parents;
+    }
+
+    public void setParents(List<Person> parents) {
+        this.parents = parents;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public Person getSpouse() {
+        return spouse;
+    }
+
+    public void setSpouse(Person spouse) {
+        this.spouse = spouse;
+    }
+
+    public boolean addChild(Person child) {
+        if (children.contains(child)) {
+            return false;
+        } else {
+            children.add(child);
+            return true;
         }
     }
 
-    // Метод для загрузки объекта FamilyTree из файла
-    @Override
-    public FamilyTree loadFamilyTreeFromFile(File file) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-            return (FamilyTree) ois.readObject();
+    public boolean addParent(Person parent) {
+        if (parents.contains(parent)) {
+            return false;
+        } else {
+            parents.add(parent);
+            return true;
         }
     }
 
-    // Метод для сохранения списка объектов Person в файл
-    @Override
-    public void savePersonsToFile(List<Person> persons, File file) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-            oos.writeObject(persons);
-        }
-    }
-
-    // Метод для загрузки списка объектов Person из файла
-    @Override
-    public List<Person> loadPersonsFromFile(File file) throws IOException, ClassNotFoundException {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
-            return (List<Person>) ois.readObject();
+    public boolean addSpouse(Person spouse) {
+        if (this.spouse != null) {
+            return false;
+        } else {
+            this.spouse = spouse;
+            return true;
         }
     }
 }
