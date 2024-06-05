d48c1cf0048978b807ef04f6b09055b8c3f74412
North <kirilichev.a@icloud.com>
2024-06-05 12:48:03 +0700

    05.06.24

---
+++ src/Human.java
@@ -0,0 +1,209 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private long id;
+    private String name;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfDeath;
+    private List<Human> parents;
+    private List<Human> children;
+    private Gender gender;
+    private Human spouse;
+
+    public Human(String name, Gender gender, LocalDate dateOfBirth, LocalDate dateOfDeath, Human father, Human mother) {
+        id = -1;
+        this.name = name;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = dateOfDeath;
+        parents = new ArrayList<>();
+        if (father != null) {
+            parents.add(father);
+        }
+        if (mother != null) {
+            parents.add(mother);
+        }
+        children = new ArrayList<>();
+    }
+
+    public Human(String name, Gender gender, LocalDate dateOfBirth) {
+        this(name, gender, dateOfBirth, null, null, null);
+    }
+
+    public Human(String name, Gender gender, LocalDate dateOfBirth, Human father, Human mother) {
+        this(name, gender, dateOfBirth, null, father, mother);
+    }
+
+    public boolean addChildren(Human child) {
+        if (!children.contains(child)) {
+            children.add(child);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addParents(Human parant) {
+        if (!parents.contains(parant)) {
+            parents.add(parant);
+            return true;
+        }
+        return false;
+    }
+
+    public Human getFather() {
+        for (Human parant : parents) {
+            if (parant.getGender() == Gender.Male) {
+                return parant;
+            }
+        }
+        return null;
+    }
+
+    public Human getMother() {
+        for (Human parent : parents) {
+            if (parent.getGender() == Gender.Female) {
+                return parent;
+            }
+        }
+        return null;
+    }
+
+    public int getAge() {
+        if (dateOfDeath == null) {
+            return getPeriod(dateOfBirth, LocalDate.now());
+        } else return getPeriod(dateOfBirth, dateOfDeath);
+    }
+
+    private int getPeriod(LocalDate dateOfBirth, LocalDate dateOfDeath) {
+        Period diff = Period.between(dateOfBirth, dateOfDeath);
+        return diff.getYears();
+    }
+
+    public void setSpouse(Human spouse) {
+        this.spouse = spouse;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setDateOfDeath(LocalDate dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Human getSpouse() {
+        return spouse;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public List<Human> getParents() {
+        return parents;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return getInfo();
+    }
+
+    public String getInfo() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("id: ");
+        stringBuilder.append(id);
+        stringBuilder.append(", имя: ");
+        stringBuilder.append(name);
+        stringBuilder.append(", пол: ");
+        stringBuilder.append(getGender());
+        stringBuilder.append(", возраст: ");
+        stringBuilder.append(getAge());
+        stringBuilder.append(", ");
+        stringBuilder.append(getSpouseInfo());
+        stringBuilder.append(", ");
+        stringBuilder.append(getFatherInfo());
+        stringBuilder.append(", ");
+        stringBuilder.append(getMotherInfo());
+        stringBuilder.append(", ");
+        stringBuilder.append(getChildrenInfo());
+        return stringBuilder.toString();
+    }
+
+    public String getSpouseInfo() {
+        String res = "супруг(а): ";
+        if (spouse == null) {
+            res += "нет";
+        } else res += spouse.getName();
+        return res;
+    }
+
+    public String getFatherInfo() {
+        String res = "отец: ";
+        Human father = getFather();
+        if (father != null) {
+            res += father.getName();
+        } else res += "неизвестен";
+        return res;
+    }
+
+    public String getMotherInfo() {
+        String res = "мать: ";
+        Human mother = getMother();
+        if (mother != null) {
+            res += mother.getName();
+        } else res += "неизвестна";
+        return res;
+    }
+
+    public String getChildrenInfo() {
+        StringBuilder res = new StringBuilder();
+        res.append("дети: ");
+        if (children.size() != 0) {
+            res.append(children.get(0).getName());
+            for (int i = 1; i < children.size(); i++) {
+                res.append(", ");
+                res.append(children.get(i).getName());
+            }
+        } else res.append("отсутствуют");
+        return res.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Human)) {
+            return false;
+        }
+        Human human = (Human) obj;
+        return human.getId() == getId();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        FamilyTree tree = new FamilyTree();
 
+        Human oleg = new Human("Олег", Gender.Male, LocalDate.of(1955, 1, 1));
+        Human tamara = new Human("Тамара", Gender.Female, LocalDate.of(1953, 9, 11), LocalDate.of(2021, 5, 15), null, null);
+
+        tree.add(oleg);
+        tree.add(tamara);
+        tree.setWedding(oleg.getId(), tamara.getId());
+
+        Human aleksandr = new Human("Александр", Gender.Male, LocalDate.of(1986, 10, 16), oleg, tamara);
+
+        tree.add(aleksandr);
+
+        Human grandFather = new Human("Петр", Gender.Male, LocalDate.of(1928, 3, 15), LocalDate.of(1991, 2, 8), null, null);
+        Human grandMother = new Human("Анна", Gender.Female, LocalDate.of(1932, 12, 19), LocalDate.of(1994, 11, 6), null, null);
+        grandFather.addChildren(tamara);
+        grandMother.addChildren(tamara);
+
+        tree.add(grandFather);
+        tree.add(grandMother);
+        tree.setWedding(grandFather.getId(), grandMother.getId());
+
+        System.out.println(tree);
     }
 }
