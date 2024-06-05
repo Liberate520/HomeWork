2ba99473aee42e0c2157b4c7ccd9aa517803cc1f
AnatoliyIB <153524711+AnatoliyIB@users.noreply.github.com>
2024-06-05 22:33:54 +0300

    hw3FamilyTreeFileManager.java

--- FamilyTreeFileManager.java
+++ FamilyTreeFileManager.java
@@ -1,33 +1,40 @@
-package homeWork;
+package homeWork.FamilyTree;
 
 import java.io.*;
 import java.util.List;
 
-public class FamilyTreeFileManager {
+import homeWork.Person.Person;
+import homeWork.Service.FileManager;
+
+public class FamilyTreeFileManager implements FileManager {
 
     // Метод для сохранения объекта FamilyTree в файл
-    public static void saveFamilyTreeToFile(FamilyTree familyTree, File file) throws IOException {
+    @Override
+    public void saveFamilyTreeToFile(FamilyTree familyTree, File file) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
             oos.writeObject(familyTree);
         }
     }
 
     // Метод для загрузки объекта FamilyTree из файла
-    public static FamilyTree loadFamilyTreeFromFile(File file) throws IOException, ClassNotFoundException {
+    @Override
+    public FamilyTree loadFamilyTreeFromFile(File file) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
             return (FamilyTree) ois.readObject();
         }
     }
 
     // Метод для сохранения списка объектов Person в файл
-    public static void savePersonsToFile(List<Person> persons, File file) throws IOException {
+    @Override
+    public void savePersonsToFile(List<Person> persons, File file) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
             oos.writeObject(persons);
         }
     }
 
     // Метод для загрузки списка объектов Person из файла
-    public static List<Person> loadPersonsFromFile(File file) throws IOException, ClassNotFoundException {
+    @Override
+    public List<Person> loadPersonsFromFile(File file) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
             return (List<Person>) ois.readObject();
         }
