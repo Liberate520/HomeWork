0bdb9d509381e67659ba029a77e68aa980da74ed
Ilya Lyalin <lyalin315@gmail.com>
2024-06-07 17:21:15 +0300

    hw4 v2 (Lyalin)

--- src/ru/gb/family_tree/Main.java
+++ src/ru/gb/family_tree/Main.java
@@ -4,8 +4,8 @@ package ru.gb.family_tree;
 
 import ru.gb.family_tree.humans.Human;
 import ru.gb.family_tree.tree.FamilyTree;
-import ru.gb.family_tree.tree.TreeNode;
 
+import javax.swing.tree.TreeNode;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -43,21 +43,21 @@ public class Main {
     }
 
     private static FamilyTree testTree () {
-        FamilyTree tree = new FamilyTree();
-        Human hum1 = new Human("Pavel", Male, LocalDate.of(1973, 11, 3));
-        Human hum2 = new Human("Olga", Female, LocalDate.of(1980, 9, 12));
+        FamilyTree tree = new FamilyTree<>();
+        Human hum1 = new Human<>("Pavel", Male, LocalDate.of(1973, 11, 3));
+        Human hum2 = new Human<>("Olga", Female, LocalDate.of(1980, 9, 12));
 
         tree.add(hum1);
         tree.add(hum2);
         tree.setWedding(hum1.getId(), hum2.getId());
 
-        Human hum3 = new Human("Nastya", Female, LocalDate.of(2000, 8, 5), hum1, hum2);
-        Human hum4 = new Human("Gleb", Male, LocalDate.of(1998, 3, 15), hum1, hum2);
+        Human hum3 = new Human<>("Nastya", Female, LocalDate.of(2000, 8, 5), hum1, hum2);
+        Human hum4 = new Human<>("Gleb", Male, LocalDate.of(1998, 3, 15), hum1, hum2);
 
         tree.add(hum3);
         tree.add(hum4);
 
-        Human hum5 = new Human("Veronika", Female, LocalDate.of(1960, 11, 26));
+        Human hum5 = new Human<>("Veronika", Female, LocalDate.of(1960, 11, 26));
         hum5.addChild(hum1);
         tree.add(hum5);
         //System.out.println(familyTree);
--- src/ru/gb/family_tree/humans/Human.java
+++ src/ru/gb/family_tree/humans/Human.java
@@ -1,12 +1,14 @@
 package ru.gb.family_tree.humans;
 
+import ru.gb.family_tree.tree.TreeNode;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Human implements Serializable, Comparable<Human> {
+public class Human<E extends TreeNode<E>> implements Serializable, Comparable<Human> {
     private long id;
     private String name;
     private Gender gender;
