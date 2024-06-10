61fec302cad0bb0070f4b92690a9a15dc939d8c8
Viktor Golev <vwpiligrim@gmail.com>
2024-06-10 23:10:51 +0300

    for the second time rewrote completely the logic of the RelationshipSearch class in the Family_Tree_HW3 branch

--- src/services/RelationshipSearch.java
+++ src/services/RelationshipSearch.java
@@ -1,65 +1,37 @@
 package services;
 
 import models.Human;
+import utils.List;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Comparator;
 
 /**
- * Класс для поиска родственных связей между людьми в генеалогическом древе.
+ * Класс RelationshipSearch предоставляет методы для поиска и сортировки родственных связей в генеалогическом древе.
  */
 public class RelationshipSearch {
-
     /**
-     * Возвращает список братьев и сестер человека.
+     * Сортирует список людей по имени в алфавитном порядке.
      *
-     * @param human Человек, для которого нужно найти братьев и сестер.
-     * @return Список братьев и сестер.
+     * @param familyMembers список людей для сортировки
+     * @return отсортированный список людей
      */
-    public static List<Human> findSiblings(Human human) {
-        List<Human> siblings = new ArrayList<>();
-        for (Human mother : human.getMothers()) {
-            siblings.addAll(mother.getChildren());
-        }
-        for (Human father : human.getFathers()) {
-            siblings.addAll(father.getChildren());
-        }
-        siblings.remove(human);
-        return siblings;
+    public List<Human> sortByName(List<Human> familyMembers) {
+        List<Human> sortedList = new List<>();
+        sortedList.addAll(familyMembers.getElements());
+        sortedList.getElements().sort(Comparator.comparing(Human::getName));
+        return sortedList;
     }
 
     /**
-     * Возвращает список дядей и тетей человека.
+     * Сортирует список людей по дате рождения.
      *
-     * @param human Человек, для которого нужно найти дядей и тетей.
-     * @return Список дядей и тетей.
+     * @param familyMembers список людей для сортировки
+     * @return отсортированный список людей
      */
-    public static List<Human> findAuntsAndUncles(Human human) {
-        List<Human> auntsAndUncles = new ArrayList<>();
-        for (Human mother : human.getMothers()) {
-            auntsAndUncles.addAll(findSiblings(mother));
-        }
-        for (Human father : human.getFathers()) {
-            auntsAndUncles.addAll(findSiblings(father));
-        }
-        return auntsAndUncles;
-    }
-
-    /**
-     * Возвращает список двоюродных братьев и сестер человека.
-     *
-     * @param human Человек, для которого нужно найти двоюродных братьев и сестер.
-     * @return Список двоюродных братьев и сестер.
-     */
-    public static List<Human> findCousinsSiblings(Human human) {
-        List<Human> cousins = new ArrayList<>();
-        for (Human aunt : findAuntsAndUncles(human)) {
-            cousins.addAll(aunt.getChildren());
-        }
-        for (Human uncle : findAuntsAndUncles(human)) {
-            cousins.addAll(uncle.getChildren());
-        }
-        cousins.removeAll(findSiblings(human));
-        return cousins;
+    public List<Human> sortByBirthDate(List<Human> familyMembers) {
+        List<Human> sortedList = new List<>();
+        sortedList.addAll(familyMembers.getElements());
+        sortedList.getElements().sort(Comparator.comparing(Human::getBirthDate));
+        return sortedList;
     }
 }
