cdf4c0fba7b85763ab3e6f717673b1bab8d757a9
kandr9 <kandr13579@gmail.com>
2024-06-12 06:30:23 +0300

    Add

--- src/ru/gb/FamilyTree/FileHandler.java
+++ src/ru/gb/FamilyTree/FileHandler.java
@@ -1,20 +1,36 @@
 package ru.gb.FamilyTree;
-
 import java.io.*;
-import java.nio.channels.WritableByteChannel;
 
 public  class FileHandler implements Writable {
-    @Override
-    public void saveFileByte(ru.gb.FamilyTree.FamilyTree familyTree) throws IOException {
-        ObjectOutputStream  objectOutputStream = new ObjectOutputStream
-                (new FileOutputStream("family.out"));
-        objectOutputStream.writeObject(familyTree);
-        objectOutputStream.close();
+    //    @Override
+//    public void saveFileByte(ru.gb.FamilyTree.FamilyTree familyTree) throws IOException {
+//        ObjectOutputStream  objectOutputStream = new ObjectOutputStream
+//                (new FileOutputStream("family.out"));
+//        objectOutputStream.writeObject(familyTree);
+//        objectOutputStream.close();
+//    }
+//    public FamilyTree loadFileBite() throws IOException, ClassNotFoundException {
+//        ObjectInputStream objectInputStream = new ObjectInputStream
+//                (new FileInputStream("family.out"));
+//        FamilyTree familyTree =(FamilyTree) objectInputStream.readObject();
+//        return familyTree;
+//    }
+    public boolean save(Serializable serializable, String filePath) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
+            objectOutputStream.writeObject(serializable);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-    public FamilyTree loadFileBite() throws IOException, ClassNotFoundException {
-        ObjectInputStream objectInputStream = new ObjectInputStream
-                (new FileInputStream("family.out"));
-        FamilyTree familyTree =(FamilyTree) objectInputStream.readObject();
-        return familyTree;
+
+    public Object read(String filePath) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filePath))) {
+            return objectInputStream.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/ru/gb/FamilyTree/Writable.java
+++ src/ru/gb/FamilyTree/Writable.java
@@ -1,10 +1,11 @@
 package ru.gb.FamilyTree;
 
-
-import java.io.IOException;
+import java.io.Serializable;
+//import java.io.IOException;
 
 public interface Writable {
-    public void saveFileByte(FamilyTree familyTree) throws IOException;
-    public  FamilyTree loadFileByte() throws IOException, ClassNotFoundException;
-
+//    public void saveFileByte(FamilyTree familyTree) throws IOException;
+//    public  FamilyTree loadFileByte() throws IOException, ClassNotFoundException;
+    boolean save(Serializable serializable, String filePath);
+    Object read(String filePath);
 }
