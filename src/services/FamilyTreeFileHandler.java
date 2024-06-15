57066c470fa4659b35c383b7510c3d0786ae6bcd
Viktor Golev <vwpiligrim@gmail.com>
2024-06-15 19:54:29 +0300

    rewrote completely the logic of the FamilyTreeFileHandler class in the Family_Tree_HW3 branch

--- src/services/FamilyTreeFileHandler.java
+++ src/services/FamilyTreeFileHandler.java
@@ -1,20 +1,22 @@
 package services;
 
-import java.io.*;
 import models.FamilyTree;
+
+import java.io.*;
 
 /**
  * Класс FamilyTreeFileHandler обрабатывает файлы с генеалогическими древами.
  * Он обеспечивает сохранение и загрузку генеалогических древ в файлы.
  */
-public class FamilyTreeFileHandler implements Serializable {
+public class FamilyTreeFileHandler {
     /**
      * Сохраняет генеалогическое древо в файл.
      *
      * @param familyTree генеалогическое древо
-     * @param filename  имя файла
+     * @param filename   имя файла
+     * @param <T>        тип объектов в генеалогическом древе
      */
-    public <T extends Serializable> void saveFamilyTree(FamilyTree<T> familyTree, String filename) {
+    public <T extends Serializable & Comparable<T>> void saveFamilyTree(FamilyTree<T> familyTree, String filename) {
         try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename))) {
             outputStream.writeObject(familyTree);
         } catch (IOException e) {
@@ -26,9 +28,11 @@ public class FamilyTreeFileHandler implements Serializable {
      * Загружает генеалогическое древо из файла.
      *
      * @param filename имя файла
+     * @param <T>       тип объектов в генеалогическом древе
      * @return загруженное генеалогическое древо
      */
-    public <T extends Serializable> FamilyTree<T> loadFamilyTree(String filename) {
+    @SuppressWarnings("unchecked")
+    public <T extends Serializable & Comparable<T>> FamilyTree<T> loadFamilyTree(String filename) {
         try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(filename))) {
             return (FamilyTree<T>) inputStream.readObject();
         } catch (IOException | ClassNotFoundException e) {
