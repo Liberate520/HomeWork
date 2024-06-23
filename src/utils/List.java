fce087e14820d7aaa5b94725bf15b4a175986f19
Viktor Golev <vwpiligrim@gmail.com>
2024-06-23 18:52:56 +0300

    rewrote completely the logic of the List class in the Family_Tree_HW3 branch

--- src/utils/List.java
+++ src/utils/List.java
@@ -2,9 +2,13 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 /**
- * Класс List обеспечивает работу с списком объектов.
+ * Класс List обеспечивает работу со списком объектов типа T.
+ * Он реализует интерфейс Iterable<T>, что позволяет использовать его в циклах for-each.
+ *
+ * @param <T> тип объектов, которые будут храниться в списке
  */
 public class List<T> implements Iterable<T> {
     private java.util.List<T> list;
@@ -42,5 +46,24 @@ public class List<T> implements Iterable<T> {
     @Override
     public Iterator<T> iterator() {
         return list.iterator();
+    }
+
+    /**
+     * Возвращает листовой итератор для списка объектов.
+     *
+     * @return листовой итератор для списка объектов
+     */
+    public ListIterator<T> listIterator() {
+        return list.listIterator();
+    }
+
+    /**
+     * Возвращает листовой итератор для списка объектов, начиная с указанной позиции.
+     *
+     * @param index начальная позиция для листового итератора
+     * @return листовой итератор для списка объектов
+     */
+    public ListIterator<T> listIterator(int index) {
+        return list.listIterator(index);
     }
 }
