a2229ed634955c2a49fc92857455d3a1709e62c9
Sergey Protopopov <protopopov_s@mail.ru>
2024-06-23 17:14:04 +0300

    Homework01 familyTree MVP finished (hometask5)

--- src/family_tree/presenter/Presenter.java
+++ src/family_tree/presenter/Presenter.java
@@ -1,11 +1,34 @@
 package family_tree.presenter;
 
+import family_tree.model.family_tree.FamilyTree;
+import family_tree.model.human.Human;
 import family_tree.view.View;
 
 public class Presenter {
     private View view;
+    private FamilyTree newTree;
 
     public Presenter(View view) {
         this.view = view;
+    }
+
+    public void add(Human human) {
+        newTree.add(human);
+        printTree();
+    }
+
+    public void printTree() {
+        String answer = newTree.getInfo();
+        view.printAnswer(answer);
+    }
+
+    public void sortByName() {
+        newTree.sortName();
+        printTree();
+    }
+
+    public void sortBySurName() {
+        newTree.sortSurName();
+        printTree();
     }
 }
