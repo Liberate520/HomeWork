02506d6bacc85707c9aafe979ac20e9abb987a32
«DonRumata» <sinjit89231515468@gmail.com>
2024-06-25 06:31:21 +0700

    OOP_5HWv.4

--- src/ru/gb/family_tree/view/ConsoleUI.java
+++ src/ru/gb/family_tree/view/ConsoleUI.java
@@ -3,76 +3,112 @@ package ru.gb.family_tree.view;
 import ru.gb.family_tree.presenter.Presenter;
 
 import java.time.LocalDateTime;
-import java.util.Locale;
 import java.util.Scanner;
 
-public class ConsoleUI implements View{
+public class ConsoleUI implements View {
     private Scanner scanner;
     private Presenter presenter;
 
-    public ConsoleUI(){
+    public ConsoleUI() {
         scanner = new Scanner(System.in);
         presenter = new Presenter(this);
     }
 
     @Override
     public void start() {
-        System.out.println("\033 [H\033 [2J");
-        int a = LocalDateTime.now().getHour();
-        if (a >= 5 && a < 12) {
+        clearConsole();
+        userHourHi();
+        String choice1, choice2;
+        do {
+            showMainMenu();
+            choice1 = scanner.nextLine();
+            switch (choice1) {
+                case "1":
+                    clearConsole();
+                    showEditMenu();
+                    choice2 = scanner.nextLine();
+                    handleEditMenuChoice(choice2);
+                    break;
+                case "2":
+                    clearConsole();
+                    showTreeMenu();
+                    choice2 = scanner.nextLine();
+                    handleTreeMenuChoice(choice2);
+                    break;
+                case "3":
+                    clearConsole();
+                    userHourBye();
+                    break;
+                default:
+                    System.out.println("Некорректный выбор, попробуйте ещё раз!");
+            }
+        } while (!"3".equals(choice1));
+    }
+
+    private void clearConsole() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private void userHourHi() {
+        int hour = LocalDateTime.now().getHour();
+        if (hour >= 5 && hour < 12) {
             System.out.println("Доброе утро!");
-        } else if (a >= 12 && a < 18) {
+        } else if (hour >= 12 && hour < 18) {
             System.out.println("Добрый день!");
-        } else if (a >= 18 && a <= 23) {
-            System.out.println("Добрый Вечер!");
+        } else if (hour >= 18 && hour <= 23) {
+            System.out.println("Добрый вечер!");
         } else {
             System.out.println("Добрая ночь!");
         }
-        String choise1, choise2, choise3;
-        do {
-            System.out.println("Выберите раздел:");
-            System.out.println("1. Редактирование элементов");
-            System.out.println("2. Редактирование дерева");
-            System.out.println("3. Завершить работу");
-            choise1 = scanner.nextLine();
-            switch (choise1) {
-                case "1":
-                    System.out.println("\033 [H\033 [2J");
-                    System.out.println("Выберите команду:");
-                    System.out.println("1. Создать шаблон");
-                    System.out.println("2. Удалить шаблон");
-                    System.out.println("3. Дополнить шаблон");
-                    System.out.println("3. Посмотреть список шаблонов");
-
-                    choise2 = scanner.nextLine();
-                case "2":
-                    System.out.println("\033 [H\033 [2J");
-                    System.out.println("Выберите команду:");
-                    System.out.println("1. Добавить шаблон в дерево:");
-                    System.out.println("2. Удалить шаблон из дерева:");
-                    System.out.println("3. Выбрать метод сортировки:");
-                    System.out.println("4. Посмотреть текущее дерево:");
-                case "3":
-                    System.out.println("\033 [H\033 [2J");
-                    int a = LocalDateTime.now().getHour();
-                    if (a >= 5 && a < 12) {
-                        System.out.println("Доброго утра!");
-                    } else if (a >= 12 && a < 18) {
-                        System.out.println("Доброго дня!");
-                    } else if (a >= 18 && a <= 23) {
-                        System.out.println("Доброго вечера!");
-                    } else {
-                        System.out.println("Доброй ночи!");
-                    }
-                default:
-                    System.out.println("Некорректный выбор, попробуйте ещё раз!");
-                    choise1 ="999";
-            }
-        } while (choise1 != "999");
-
     }
+
+    private void userHourBye() {
+        int hour = LocalDateTime.now().getHour();
+        if (hour >= 5 && hour < 12) {
+            System.out.println("Доброго утра!");
+        } else if (hour >= 12 && hour < 18) {
+            System.out.println("Доброго дня!");
+        } else if (hour >= 18 && hour <= 23) {
+            System.out.println("Доброго вечера!");
+        } else {
+            System.out.println("Доброй ночи!");
+        }
+    }
+
+    private void showMainMenu() {
+        System.out.println("Выберите раздел:");
+        System.out.println("1. Редактирование элементов");
+        System.out.println("2. Редактирование дерева");
+        System.out.println("3. Завершить работу");
+    }
+
+    private void showEditMenu() {
+        System.out.println("Выберите команду:");
+        System.out.println("1. Создать шаблон");
+        System.out.println("2. Удалить шаблон");
+        System.out.println("3. Дополнить шаблон");
+        System.out.println("4. Посмотреть список шаблонов");
+    }
+
+    private void handleEditMenuChoice(String choice) {
+        // Здесь должна быть логика обработки выбора пользователя
+    }
+
+    private void showTreeMenu() {
+        System.out.println("Выберите команду:");
+        System.out.println("1. Добавить шаблон в дерево");
+        System.out.println("2. Удалить шаблон из дерева");
+        System.out.println("3. Выбрать метод сортировки");
+        System.out.println("4. Посмотреть текущее дерево");
+    }
+
+    private void handleTreeMenuChoice(String choice) {
+        // Здесь должна быть логика обработки выбора пользователя
+    }
+
     @Override
     public void printAnswer(String answer) {
-
+        System.out.println(answer);
     }
 }
