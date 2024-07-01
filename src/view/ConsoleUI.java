4dfcef903d362f1433da1980207b16062ba493b2
Anton Sopov <recorrd@yandex.ru>
2024-07-01 13:52:21 +0300

    Edits

--- src/view/ConsoleUI.java
+++ src/view/ConsoleUI.java
@@ -17,14 +17,7 @@ public class ConsoleUI implements View {
     private Menu startMenu;
     private String filePatch;
 
-    /**
-     * Конструктор класса
-     * создает новый scanner
-     * создает новый presenter и передает текущий view
-     * создает новый workMenu
-     * создает новое startMenu и передает ему текущий consoleUI
-     * инициализирует filePatch (путь сохранения и загрузки дерева)
-     */
+
     public ConsoleUI() {
         scanner = new Scanner(System.in);
         presenter = new Presenter(this);
@@ -39,13 +32,6 @@ public class ConsoleUI implements View {
         launchingMenu(startMenu);
     }
 
-    /**
-     * Принимает клаас Menu
-     * Добавляет в workMenu true в начале работы удаляет при завершении
-     * в цикле отображает меню при помощи метода showMenu
-     *
-     * @param menu
-     */
     private void launchingMenu(Menu menu) {
         workMenu.add(true);
         while (workMenu.getLast()) {
@@ -63,62 +49,17 @@ public class ConsoleUI implements View {
         System.out.println(answer);
     }
 
-    /**
-     * выводит справку по использованию меню в консоль
-     */
-    public void callingHelp() {
-        System.out.println(
-                "******************* Справка ******************\n" +
-                        "*               Структура меню               *\n" +
-                        "* 0. - Вызывает эту справку *\n" +
-                        "* 1. - Загружает дерево из файла по адресу: src/data_save/familyTree.txt *\n" +
-                        "* 2. - Сохраняет дерево в файл по адресу: src/data_save/familyTree.txt *\n" +
-                        "* 3. - Запрашивает данные для добавления и добавляет человека в дерево *\n" +
-                        "* 4. - Запрашивает данные для поиска и вызывает меню действий с результатом поиска *\n" +
-                        "*     0. - Вызывает эту справку *\n" +
-                        "*     1. - Выводит список людей для выбора родителей и устанавливает указанных родителей, а им устанавливает ребенка *\n" +
-                        "*     2. - Выводит на экран возраст *\n" +
-                        "*     3. - Выводит на экран родителей *\n" +
-                        "*     4. - Выводит на экран детей *\n" +
-                        "*     5. - Выводит на экран бабушек и дедушек *\n" +
-                        "*     6. - Выводит на экран внуков *\n" +
-                        "*     7. - Выходит в предыдущее меню *\n" +
-                        "* 5. - Вызывает меню выбора сортировок дерева *\n" +
-                        "*     0. - Вызывает эту справку *\n" +
-                        "*     1. - Сортирует дерево по имени и показывает его краткую форму *\n" +
-                        "*     2. - Сортирует дерево по возрасту и показывает его краткую форму *\n" +
-                        "*     3. - Сортирует дерево по количеству детей и показывает его краткую форму *\n" +
-                        "*     4. - Выходит в предыдущее меню *\n" +
-                        "* 6. - Вызывает меню выбора печати дерева *\n" +
-                        "*     0. - Вызывает эту справку *\n" +
-                        "*     1. - Печатает  краткую форму джерева ФИО, ворзаст и количество детей *\n" +
-                        "*     2. - Печатает полную форму дерева *\n" +
-                        "*     3. - Выходит в предыдущее меню *\n" +
-                        "* 7. - Выходит из программы *\n"
-        );
-    }
 
-    /**
-     * Вызывает у презентора метод loadingTree передавая ему filePatch
-     */
     public void loadingTree() {
         presenter.loadingTree(filePatch);
     }
 
-    /**
-     * Вызывает у презентора метод savingTree передавая ему filePatch
-     */
+
     public void savingTree() {
         presenter.savingTree(filePatch);
     }
 
-    /**
-     * Запрашивает у пользователя name методом enteringName
-     * Запрашивает у пользователя gender методом enteringGender
-     * Запрашивает у пользователя dob методом enteringDataStr заперщает возвращать null
-     * Запрашивает у пользователя dod методом enteringDataStr разрешает возвращать null
-     * Dspsdftn vtnjl addHuman в presenter и передает ему собранные аргументы
-     */
+
     public void addHuman() {
         String name;
         Gender gender;
@@ -138,12 +79,7 @@ public class ConsoleUI implements View {
         presenter.addHuman(name, gender, birthDate, deathDate);
     }
 
-    /**
-     * Запрашивает у пользователя строки
-     * Вызывает в presenter метод findHuman с параметром веденным пользователем
-     * Если возвращается true запускает меню АctionFindMenu для работы с найденным человеком
-     * Если возвращается false печатает сообщение
-     */
+
     public void findHuman() {
         String name;
         System.out.println("Введите ФИО или его часть: ");
@@ -156,58 +92,33 @@ public class ConsoleUI implements View {
         }
     }
 
-    /**
-     * создает и запускает методом launchingMenu меню SortMenu
-     */
     public void choicingSort() {
         Menu sortMenu = new SortMenu(this);
         launchingMenu(sortMenu);
     }
 
-    /**
-     * создает и запускает методом launchingMenu меню PrintMenu
-     */
+
     public void choicingPrint() {
         Menu printMenu = new PrintMenu(this);
         launchingMenu(printMenu);
     }
 
-    /**
-     * Устанавливает  последний елемент workMenu false
-     */
+
     public void goOut() {
         workMenu.set(workMenu.size() - 1, false);
     }
 
-    /**
-     * Принимает строку
-     * проверяет состоит ли строка только из цифр
-     *
-     * @param choiceStr
-     * @return boolean
-     */
+
     private boolean checkingIsDigital(String choiceStr) {
         return (choiceStr.matches("\\d+"));
     }
 
-    /**
-     * Печатает ошибку с указанием диапазона допустимых значений
-     *
-     * @param min
-     * @param max
-     */
+
     private void printError(int min, int max) {
         System.out.printf("Вы ввели не коретные данные \nВведите число от %d до %d\n", min, max);
     }
 
-    /**
-     * Через обращение в presenter shortPrintTree выводит список людей в дереве
-     * запрашивает выбор пользователя для мамы
-     * Через обращение в presenter shortPrintTree выводит список людей в дереве
-     * запрашивает выбор пользователя для папы
-     * Если введены значение коректны вызывает через presenter метод setParents передавая индексы мамы и папы в дереве
-     * Вслучаее не коректных вводов печатает сообщение
-     */
+
     public void setParents() {
         int indexMother, indexFather;
         System.out.println("Выберите маму: \n");
@@ -223,96 +134,63 @@ public class ConsoleUI implements View {
         }
     }
 
-    /**
-     * Вызвает в presenter метод getAge
-     */
+
     public void getAge() {
         presenter.getAge();
     }
 
-    /**
-     * Вызвает в presenter метод getParents
-     */
+
     public void getParents() {
         presenter.getParents();
     }
 
-    /**
-     * Вызвает в presenter метод getChildren
-     */
+
     public void getChildren() {
         presenter.getChildren();
     }
 
-    /**
-     * Вызвает в presenter метод getGrandparents
-     */
+
     public void getGrandparents() {
         presenter.getGrandparents();
     }
 
-    /**
-     * Вызвает в presenter метод getGrandsons
-     */
+
     public void getGrandsons() {
         presenter.getGrandsons();
     }
 
-    /**
-     * Вызвает в presenter метод sortName
-     * Печаате краткое дерево через обращение в presenter методом shortPrintTree
-     */
+
     public void sortName() {
         presenter.sortName();
         System.out.println("соритровка по имени");
         System.out.println(presenter.shortPrintTree());
     }
 
-    /**
-     * Вызвает в presenter метод sortAge
-     * Печаате краткое дерево через обращение в presenter методом shortPrintTree
-     */
+
     public void sortAge() {
         presenter.sortAge();
         System.out.println("соритровка по возрасту");
         System.out.println(presenter.shortPrintTree());
     }
 
-    /**
-     * Вызвает в presenter метод sortNumberChildren
-     * Печаате краткое дерево через обращение в presenter методом shortPrintTree
-     */
+
     public void sortNumberChildren() {
         presenter.sortNumberChildren();
         System.out.println("соритровка по количеству детей");
         System.out.println(presenter.shortPrintTree());
     }
 
-    /**
-     * Печаате краткое дерево через обращение в presenter методом longPrintTree
-     */
+
     public void longPrint() {
         System.out.println(presenter.longPrintTree());
     }
 
-    /**
-     * Печаате краткое дерево через обращение в presenter методом shortPrintTree
-     */
+
     public void shortPrint() {
         System.out.println(presenter.shortPrintTree());
     }
 
-    /**
-     * принимает два int дипазона доступного выбора включительно
-     * принимает от пользователя строку
-     * проверяет его checkingIsDigital на толькочисла
-     * если да то пребразует в int если нет то печатает ошибку через printError и возращает -1
-     * проверчяет число на нахождение в диапазоне
-     * сли да то возращает его int если нет то печатает ошибку через printError и возращает -1
-     * @param min
-     * @param max
-     * @return
-     */
+
     private int chekingCoice(int min, int max) {
         String choiceStr = scanner.nextLine();
         if (checkingIsDigital(choiceStr)) {
@@ -328,12 +206,7 @@ public class ConsoleUI implements View {
         return -1;
     }
 
-    /**
-     * Запрашивает ФИО проверяет что строка не пустая
-     * возвращает если если строка непустая
-     * при пустой строке посторяет запрос.
-     * @return String
-     */
+
     private String enteringName() {
         String name;
         boolean repeatQuestion;
@@ -351,11 +224,7 @@ public class ConsoleUI implements View {
         return name;
     }
 
-    /**
-     * Запрашивает пол у пользователя ввиде строки из одногосимвола м или ж (регистр не важен)
-     * воаращает Gender в зависимости от пола, если выбор не соотвествует значения повторяет запрос и выводит подсказку
-     * @return Gender
-     */
+
     private Gender enteringGender() {
         String genderStr;
         System.out.println("Укажите пол: М или Ж");
@@ -373,16 +242,7 @@ public class ConsoleUI implements View {
         } while (true);
     }
 
-    /**
-     * принимает boolean truе разрешает вовращать null, false - запрещает
-     * Запрашивает у пользователя дату ввиде строки и вормирует из нее формат ГГГГ-ММ-ДД
-     * запрашивает год в промежктке от 1000 до 2100
-     * запрашивает месяц в промежутке от 1 до 12 значения меньше 10 дополняются 0 перед числом
-     * запрашивает день в промежутке от 1 до 31 значения меньше 10 дополняются 0 перед числом
-     * возвращает отформатированную строку или null
-     * @param allowNull boolean
-     * @return
-     */
+
     private String enteringDataStr(boolean allowNull) {
         StringBuilder date = new StringBuilder();
         String temp;
--- src/view/menu/Menu.java
+++ src/view/menu/Menu.java
@@ -8,12 +8,6 @@ public abstract class Menu {
     protected List<Command> commands;
     protected String menuName;
 
-    /**
-     * Создает StringBuilder добавляет в него название меню  и список команд из передаваемого меню,
-     * а также визуальный разделитьль из *
-     * возвращает результат преобразованный в строку
-     * @return String
-     */
     public String showMenu() {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("------------------------\n");
@@ -29,18 +23,10 @@ public abstract class Menu {
         return stringBuilder.toString();
     }
 
-    /**
-     * Получает int и выполняет метод execute в команде из массива по этому индексу
-     * @param choice
-     */
     public void execute (int choice) {
         commands.get(choice).execute();
     }
 
-    /**
-     * ВОзвращает список команд commands
-     * @return
-     */
     public List<Command> getCommands() {
         return commands;
     }
