9301c101819ffc41b01d86742a0534e129e08d47
Viktor Golev <vwpiligrim@gmail.com>
2024-07-05 10:59:25 +0300

    rewrote completely the logic of the Main class in the Family_Tree_HW7 branch

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
-import models.Gender;
-import models.Human;
+package main;
+
 import presenters.Presenter;
 import services.FamilyTreeService;
+import services.FamilyTreeServiceImpl;
 import views.ConsoleView;
 import views.View;
 
 /**
  * Класс Main является точкой входа в приложение для работы с генеалогическим древом.
+ * В классе Main реализован паттерн MVP, где View запускается и пользователь взаимодействует с ней.
  */
 public class Main {
     /**
@@ -17,35 +19,10 @@ public class Main {
     public static void main(String[] args) {
         // Создание объектов представления, сервиса и презентера
         View view = new ConsoleView();
-        FamilyTreeService familyTreeService = new FamilyTreeService();
+        FamilyTreeService familyTreeService = new FamilyTreeServiceImpl();
         Presenter presenter = new Presenter(view, familyTreeService);
 
-        // Добавление людей в генеалогическое древо
-        presenter.addHuman(new Human("Иоанн Алексеевич Романов", "06.09.1666", "08.02.1696", Gender.MALE));
-        presenter.addHuman(new Human("Алексей Михайлович Романов", "19.03.1629", "8.02.1676", Gender.MALE));
-        presenter.addHuman(new Human("Мария Ильинична Милославская", "11.04.1624", "13.03.1669", Gender.FEMALE));
-        presenter.addHuman(new Human("Дмитрий Алексеевич Романов", "01.11.1648", "16.10.1649", Gender.MALE));
-        presenter.addHuman(new Human("Евдокия Алексеевна Романова", "17.02.1650", "10.05.1712", Gender.FEMALE));
-        presenter.addHuman(new Human("Прасковья Фёдоровна Салтыкова", "22.10.1664", "24.10.1723", Gender.FEMALE));
-        presenter.addHuman(new Human("Мария Ивановна Романова", "31.03.1689", "23.02.1692", Gender.FEMALE));
-        presenter.addHuman(new Human("Феодосия Ивановна Романова", "14.06.1690", "22.05.1691", Gender.FEMALE));
-        presenter.addHuman(new Human("Екатерина Иоанновна Романова", "8.11.1691", "25.06.1733", Gender.FEMALE));
-        presenter.addHuman(new Human("Анна Иоанновна Романова", "07.02.1693", "28.10.1740", Gender.FEMALE));
-        presenter.addHuman(new Human("Прасковья Иоанновна Романова", "04.10.1694", "19.10.1731", Gender.FEMALE));
-
-        // Вывод неотсортированных данных
-        presenter.displayFamilyTree();
-
-        // Вывод сортированных данных по имени
-        presenter.sortByName();
-
-        // Вывод сортированных данных по дате рождения
-        presenter.sortByBirthDate();
-
-        // Сохранение данных в файл
-        presenter.saveToFile("family_tree.txt");
-
-        // Загрузка данных из файла
-        presenter.loadFromFile("family_tree.txt");
+        // Запуск приложения
+        view.start(presenter);
     }
 }
