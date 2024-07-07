9463af2de4c46efa91a27c379d719d282a4a0382
BayborodinAlexandr <albayborodin@yandex.ru>
2024-07-08 00:13:06 +0300

    Добавлены файлы домашней работы

---
+++ src/ru/gb/family_tree/family_tree/FamilyTree.java
@@ -0,0 +1,42 @@
+package ru.gb.family_tree.family_tree;
+
+import ru.gb.family_tree.human.Human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTree {
+    private List<Human> humans;
+
+    public FamilyTree() {
+        humans = new ArrayList<>();
+    }
+
+    public Human getReadName(String nameHuman) {
+        for (Human human : humans) {
+            if (human.getName().equals(nameHuman)) {
+                return human;
+            }
+        }
+        return null;
+    }
+
+    public void addHuman(Human human) {
+        humans.add(human);
+    }
+
+    public List<Human> getHumans() {
+        return humans;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Список людей:");
+        for (Human human : humans) {
+            stringBuilder.append("\n\n");
+            stringBuilder.append(human.getBasicInfo());
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ src/ru/gb/family_tree/human/Human.java
@@ -0,0 +1,94 @@
+package ru.gb.family_tree.human;
+
+import ru.gb.family_tree.gender.Gender;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human {
+    private String name;
+    private LocalDate dateOfBirth, dateOfDeath;
+    private Gender gender;
+    private Human father, mother;
+    private List<Human> children;
+
+    public Human(String name, LocalDate dateOfBirth, Gender gender, Human father, Human mother) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.father = father;
+        this.mother = mother;
+        this.children = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfDeath(LocalDate dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public LocalDate getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public void setFather(Human father) {
+        this.father = father;
+    }
+
+    public Human getMother() {
+        return mother;
+    }
+
+    public void setMother(Human mother) {
+        this.mother = mother;
+    }
+
+    public List<Human> getChildren() {
+        return children;
+    }
+
+    public void addChild(Human child) {
+        this.children.add(child);
+    }
+
+    public String getChildrenAsString() {
+        if (children.isEmpty()) {
+            return "нет";
+        }
+        StringBuilder childrenNames = new StringBuilder();
+        for (Human child : children) {
+            if (childrenNames.length() > 0) {
+                childrenNames.append(", ");
+            }
+            childrenNames.append(child.getName());
+        }
+        return childrenNames.toString();
+    }
+
+    public String getBasicInfo() {
+        return "Имя: " + name +
+                ", пол: " + (gender == Gender.Male ? "мужской" : "женский") +
+                ", дата рождения: " + dateOfBirth +
+                ", дата смерти: " + (dateOfDeath != null ? dateOfDeath : (gender == Gender.Male ? "жив" : "жива"));
+    }
+
+    @Override
+    public String toString() {
+        return getBasicInfo() + ", дети: " + getChildrenAsString();
+    }
+}
