3dfb72b9a12a7727d37a0cf440437c8d14c5d845
alexidi <alex.vyrlan@gmail.com>
2024-07-09 13:57:56 +0300

    Added code that will manage the family tree and provide methods for adding, searching and researching.

--- src/FamilyTree/FamilyTree.java
+++ src/FamilyTree/FamilyTree.java
@@ -1,4 +1,38 @@
 package FamilyTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FamilyTree {
+    private List<Person> people;
+
+    public FamilyTree() {
+        this.people = new ArrayList<>();
+    }
+
+    public void addPerson(Person person) {
+        this.people.add(person);
+    }
+
+    public Person findPersonByName(String name) {
+        for (Person person : people) {
+            if (person.getName().equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+
+    public List<Person> getChildren(Person person) {
+        return person.getChildren();
+    }
+
+    public void createNewTree() {
+        this.people.clear();
+    }
+
+    public List<Person> getAllPeople() {
+        return new ArrayList<>(people);
+    }
 }
+
