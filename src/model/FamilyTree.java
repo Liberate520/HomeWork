1082148a71c6d1e483f631402d99f4874c7994e6
alexidi <alex.vyrlan@gmail.com>
2024-07-15 12:45:47 +0300

    Added modification

--- src/model/FamilyTree.java
+++ src/model/FamilyTree.java
@@ -3,7 +3,6 @@ package model;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
