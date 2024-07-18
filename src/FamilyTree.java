0bc6baa888a842845bcd6e1eebbd21d92ce54ba0
BikbaevElmir <integral@nxt.ru>
2024-07-18 16:16:08 +0500

    Удалил лишние списки

--- src/FamilyTree.java
+++ src/FamilyTree.java
@@ -5,12 +5,10 @@ public class FamilyTree {
     private int id;
     private int humanId;
     private List<Human> humans;
-    private List<Human> children;
 
     public FamilyTree(int id){
         this.id = id;
         humans = new ArrayList<>();
-        children = new ArrayList<>();
     }
     public void addHuman(Human human){
         human.setId(++humanId);
@@ -26,26 +24,22 @@ public class FamilyTree {
         return stringBuilder.toString();
     }
     public String getHumansChildrenInfo(int selectId) {
-        children.clear();
+
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Список детей для " + humans.get(selectId-1).getName() + " :\n");
 
         for(Human human : humans){
             if (human.getFather() == humans.get(selectId-1)) {
-                children.add(human);
-            }
-        }
-
-        if (children.size() > 0){
-            for (Human human : children) {
                 stringBuilder.append(" - ");
                 stringBuilder.append(human.getName());
                 stringBuilder.append("\n");
             }
-        } else{
-            stringBuilder.append("Для данного человека сведений по детям нет");
-            stringBuilder.append("\n");
         }
+
+
+            //stringBuilder.append("Для данного человека сведений по детям нет");
+           // stringBuilder.append("\n");
+
         return stringBuilder.toString();
     }
 }
