755606d61fee719e020f3b84f6774906523f3072
syuzan.qamalyan <kamalyan.syuzanna@mail.ru>
2024-07-22 00:47:06 +0400

    Домашнее задание №3 .

--- src/ru/gb/family_tree/Main.java
+++ src/ru/gb/family_tree/Main.java
@@ -34,7 +34,7 @@ public class Main {
         Human ivan = new Human ("Ivan", Gender.Male, LocalDate.of(2000, 3, 3));
         Human iana = new Human ("Iana", Gender.Female, LocalDate.of(2000, 8,9));
         Human child1 = new Human ("Elena", Gender.Female, LocalDate.of(2022,3,7));
-        Human child2 = new Human ("Igor", Gender.Male, LocalDate.of(2024, 4,4));
+        Human child2 = new Human ("Igor", Gender.Male, LocalDate.of(2024, 5,4));
         tree.add(ivan);
         tree.add(iana);
         tree.add(child1);
