96b2f6b558547faa41f62e85b743a0d3e56d8277
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-07-24 18:05:31 +0300

    изменен файл персон

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Person {
     private int id;
     private String name;
+    private Date birthDate;
+    private Gender gender;
     private Person father;
     private Person mother;
     private List<Person> children;
 
-    public Person(int id, String name) {
+    public Person(int id, String name, Date birthDate, Gender gender) {
         this.id = id;
         this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
         this.children = new ArrayList<>();
     }
 
@@ -20,6 +25,14 @@ public class Person {
 
     public String getName() {
         return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 
     public Person getFather() {
@@ -44,5 +57,28 @@ public class Person {
 
     public void addChild(Person child) {
         children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", gender=" + gender +
+                ", father=" + (father != null ? father.getName() : "null") +
+                ", mother=" + (mother != null ? mother.getName() : "null") +
+                ", children=" + childrenNames() +
+                '}';
+    }
+
+    private String childrenNames() {
+        StringBuilder names = new StringBuilder();
+        for (Person child : children) {
+            if (names.length() > 0) {
+                names.append(", ");
+            }
+            names.append(child.getName());
+        }
+        return names.toString();
     }
 }
