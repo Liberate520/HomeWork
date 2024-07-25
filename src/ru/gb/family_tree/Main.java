7978b75d39a8450e78c8c32dc0d9d083b3bc8362
Katherine <katrinker@bk.ru>
2024-07-25 15:18:30 +0700

    add writer

--- src/ru/gb/family_tree/Main.java
+++ src/ru/gb/family_tree/Main.java
@@ -3,11 +3,13 @@ package ru.gb.family_tree;
 import ru.gb.family_tree.family_tree.FamilyTree;
 import ru.gb.family_tree.human.Gender;
 import ru.gb.family_tree.human.Human;
+import ru.gb.family_tree.writer.FileHandler;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
     FamilyTree tree = new FamilyTree();
 
     Human fyodor = new Human("Фёдор", Gender.Male, LocalDate.of(1956, 8, 15));
@@ -39,6 +41,22 @@ public class Main {
 
     tree.setDivorce(fyodor, vera);
 
+
+    writeTree(tree);
     System.out.println(tree);
+
+    readTree(tree);
+
     }
+
+    private static void writeTree(FamilyTree tree) throws IOException {
+        FileHandler fileHandler = new FileHandler();
+        fileHandler.write(tree);
+    }
+
+    private static Object readTree(FamilyTree tree) throws IOException, ClassNotFoundException {
+        FileHandler fileHandler = new FileHandler();
+        return(FamilyTree) fileHandler.read();
+    }
+
 }
