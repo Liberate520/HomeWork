a9fffdf85046e5fdf003affbb9970b5ce9997b3a
avopira <153915902+avopira@users.noreply.github.com>
2024-07-31 23:48:36 +0500

    wrong commit

--- src/Family_tree/writer/Writer.java
+++
@@ -1,9 +0,0 @@
-package Family_tree.writer;
-
-// import java.io.IOException;
-import java.io.Serializable;
-
-public interface Writer {
-    Object read(); 
-    void save(Serializable serializable);
-}
