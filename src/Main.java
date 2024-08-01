d6c2f81bb9ec3cb1b9f1262b8fc231daec2433ad
Oleg Golubev <traicer95@gmail.com>
2024-08-01 17:59:14 +0300

    hw 4 final version

--- src/Main.java
+++ src/Main.java
@@ -11,29 +11,26 @@ public class Main {
 
     public static void main(String[] args) {
 //        FamilyTree tree = load();
-        Service <Human>  service= new Service<>();
-        FamilyTree <Human> ft = service.getFamilyTree();
-        service.addHuman("Leha",Gender.Male, LocalDate.of(1999,9,9));
-        service.addHuman("Oleg", Gender.Male, LocalDate.of(1995,7,2));
-        service.addHuman("Nastya", Gender.Female, LocalDate.of(2001,3,16));
-        service.addHuman("Nadezhda", Gender.Female, LocalDate.of(1974,6,26));
-        service.setWedding("Oleg", "Nastya");
-        service.addHuman("Andrey", Gender.Male, LocalDate.of(1970,7,25));
-        service.setWedding("Nadezhda", "Andrey");
-        service.addHuman("Katya", Gender.Female, LocalDate.of(2004,12,24));
-        ft.findByName("Katya").addParent(ft.findByName("Andrey"));
-        ft.findByName("Katya").addParent(ft.findByName("Nadezhda"));
-        ft.findByName("Oleg").addParent(ft.findByName("Andrey"));
-        ft.findByName("Oleg").addParent(ft.findByName("Nadezhda"));
-        ft.findByName("Nadezhda").addChild(ft.findByName("Oleg"));
-        ft.findByName("Nadezhda").addChild(ft.findByName("Katya"));
-//        ft.sortByName();
-//        ft.sortByAge();
-//        ft.sortByBirthDate();
-        save(service.getFamilyTree());
-
-        System.out.println(service.getFamilyTree());
-
+        Service <Human>  myFamilyTree= new Service<>();
+        myFamilyTree.addHuman("Leha",Gender.Male, LocalDate.of(1999,9,9));
+        myFamilyTree.addHuman("Oleg", Gender.Male, LocalDate.of(1995,7,2));
+        myFamilyTree.addHuman("Nastya", Gender.Female, LocalDate.of(2001,3,16));
+        myFamilyTree.addHuman("Nadezhda", Gender.Female, LocalDate.of(1974,6,26));
+        myFamilyTree.setWedding("Oleg", "Nastya");
+        myFamilyTree.addHuman("Andrey", Gender.Male, LocalDate.of(1970,7,25));
+        myFamilyTree.setWedding("Nadezhda", "Andrey");
+        myFamilyTree.addHuman("Katya", Gender.Female, LocalDate.of(2004,12,24));
+        myFamilyTree.addParent("Katya","Andrey");
+        myFamilyTree.addParent("Katya","Nadezhda");
+        myFamilyTree.addParent("Oleg","Andrey");
+        myFamilyTree.addParent("Oleg","Nadezhda");
+        myFamilyTree.addChild("Nadezhda","Oleg");
+        myFamilyTree.addChild("Nadezhda","Katya");
+//        myFamilyTree.sortByName();
+//        myFamilyTree.sortByAge();
+        myFamilyTree.sortByBirthDate();
+        save(myFamilyTree.getFamilyTree());
+        System.out.println(myFamilyTree.getFamilyTree());
     }
 
     private static FamilyTree load() {
--- src/family_tree/Service.java
+++ src/family_tree/Service.java
@@ -9,26 +9,39 @@ import java.time.LocalDate;
 
 public class Service<E extends FamilyTreeItem<E>>{
     private FamilyTree<E> familyTree;
-    private HumanBuilder humanBuilder;
+    private HumanBuilder<E> humanBuilder;
 
     public Service(){
-        familyTree = new FamilyTree();
-        humanBuilder = new HumanBuilder();
+        familyTree = new FamilyTree<>();
+        humanBuilder = new HumanBuilder<>();
     }
 
     public void addHuman(String name, Gender gender, LocalDate birthDate){
         E human = (E) humanBuilder.setName(name).setGender(gender).setBirthDate(birthDate).build();
         familyTree.add(human);
     }
-
     public boolean setWedding(String name1, String name2){
         return familyTree.setWedding(familyTree.findByName(name1), familyTree.findByName(name2));
     }
     public boolean setDivorce(String name1, String name2){
         return familyTree.setDivorce(familyTree.findByName(name1), familyTree.findByName(name2));
     }
-
+    public boolean addChild(String human, String child){
+        return familyTree.findByName(human).addChild(familyTree.findByName(child));
+    }
+    public boolean addParent(String human, String parent){
+        return familyTree.findByName(human).addParent(familyTree.findByName(parent));
+    }
     public FamilyTree<E> getFamilyTree() {
         return familyTree;
+    }
+    public void sortByName(){
+        familyTree.sortByName();
+    }
+    public void sortByAge(){
+        familyTree.sortByAge();
+    }
+    public void sortByBirthDate(){
+        familyTree.sortByBirthDate();
     }
 }
