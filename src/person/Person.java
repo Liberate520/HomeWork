58945786635dd43ced846e3a2e87f36e59d84940
Serzh <kserzh84@gmail.com>
2024-08-01 19:06:50 +0400

    Createв writer/save FamilyTree in fale

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,19 +1,24 @@
+package person;
+
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Person {
+public class Person implements Serializable {
     private String name;
     private LocalDate birthDate;
     private LocalDate deathDate;
+    private Gender gender;
     private List<Person> parents;
     private List<Person> children;
 
-    public Person(String name, LocalDate birthDate, LocalDate deathDate) {
+    public Person(String name, LocalDate birthDate, LocalDate deathDate, Gender gender) {
         this.name = name;
         this.birthDate = birthDate;
         this.deathDate = deathDate;
+        this.gender = gender;
         this.parents = new ArrayList<>();
         this.children = new ArrayList<>();
     }
@@ -21,6 +26,11 @@ public class Person {
     public void addChild(Person child) {
         this.children.add(child);
         child.parents.add(this);
+    }
+
+    public void addParent(Person parent) {
+        this.parents.add(parent);
+        parent.children.add(this);
     }
 
     public String getName() {
@@ -42,6 +52,6 @@ public class Person {
 
     @Override
     public String toString() {
-        return name + ", " + getAge() + " years old";
+        return name + ", " + getAge() + " years old, " + gender;
     }
 }
