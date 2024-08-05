7a0f65f5425168d982573b64b41e92b0d225250c
Gleb <gricay.gleb@mail.ru>
2024-08-05 19:45:22 +0500

    Implement Iterable interface, add sorting methods, and create package structure

--- src/main/java/com/example/familytree/util/FileOperations.java
+++
@@ -1,9 +0,0 @@
-package com.example.familytree.util;
-
-import com.example.familytree.model.FamilyTree;
-import java.io.IOException;
-
-public interface FileOperations {
-    void saveToFile(String filename) throws IOException;
-    FamilyTree loadFromFile(String filename) throws IOException, ClassNotFoundException;
-}
