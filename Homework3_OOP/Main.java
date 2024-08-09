7558ceede26f4e03cd2e586872d0b5f4700e7093
DivergenceTheorem <merjan02@gmail.com>
2024-08-09 12:05:42 +0500

    Add files via upload

---
+++ Homework3_OOP/Main.java
@@ -0,0 +1,42 @@
+package Homework2_OOP;
+
+import java.time.LocalDate;
+
+public class Main{
+
+    public static void main(String[] args) { 
+
+        
+        FamilyTree tree = famTree();
+        System.out.println(tree);
+
+    }
+
+    private static FamilyTree load() {
+        FileHandler fileHandler = new FileHandler();
+        return (FamilyTree) fileHandler.read();
+    }
+
+    private static void save(FamilyTree familyTree){
+        FileHandler fileHandler = new FileHandler();
+        fileHandler.save(familyTree);
+    }
+
+    private static FamilyTree famTree() {
+        FamilyTree tree = new FamilyTree();
+
+        Human first = new Human ("Altair", Gender.Male, LocalDate.of(1975, 2, 14));
+        Human second = new Human ("Jasmine", Gender.Female, LocalDate.of(1976, 3, 11));
+
+        tree.add(first);
+        tree.add(second);
+        tree.setWedding(first, second);
+
+        Human child1 = new Human ("Baelor", Gender.Male, LocalDate.of(2000, 1, 23), first, second);
+
+        tree.add(child1);
+        
+        tree.sortByName();
+        return tree;
+    }
+}
