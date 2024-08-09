37d44a5335cc4dd48e6f9e1e79d90985fd2aae03
Artem <vlasov.art.ser@yandex.ru>
2024-08-09 12:22:31 +0300

    Добавили инверсию зависимостей FamilyTreeConsole.java  передавая FamilyTreePresenter и FamilyTreeConsoleView в качестве зависимостей, вместо создания их внутри класса.

--- src/Family_Tree/FamilyTreeConsole.java
+++ src/Family_Tree/FamilyTreeConsole.java
@@ -5,23 +5,19 @@ import java.util.Scanner;
 
 public class FamilyTreeConsole {
     private static final Scanner scanner = new Scanner(System.in);
-    private static FamilyTreePresenter presenter;
+    private final FamilyTreePresenter presenter;
+    private final FamilyTreeConsoleView view;
 
-    public static void main(String[] args) {
-        presenter = new FamilyTreePresenter(new FamilyTree<Family_Tree.Person>(), new FamilyTreeConsoleView(presenter));
+    public FamilyTreeConsole(FamilyTreePresenter presenter, FamilyTreeConsoleView view) {
+        this.presenter = presenter;
+        this.view = view;
+    }
 
+    public void start() {
         boolean exit = false;
         while (!exit) {
-            System.out.println("Выберите опцию:");
-            System.out.println("1. Добавить человека");
-            System.out.println("2. Найти человека");
-            System.out.println("3. Сохранить дерево в файл");
-            System.out.println("4. Загрузить дерево из файла");
-            System.out.println("5. Вывести дерево");
-            System.out.println("6. Выйти");
-
-            int option = scanner.nextInt();
-            scanner.nextLine(); // Потребление символа новой строки
+            view.displayMenu();
+            int option = getUserChoice();
 
             switch (option) {
                 case 1:
@@ -48,7 +44,13 @@ public class FamilyTreeConsole {
         }
     }
 
-    private static void addPerson() {
+    private int getUserChoice() {
+        int option = scanner.nextInt();
+        scanner.nextLine(); // Потребление символа новой строки
+        return option;
+    }
+
+    private void addPerson() {
         System.out.println("Введите имя:");
         String name = scanner.nextLine();
         System.out.println("Введите пол (0 для Мужского, 1 для Женского):");
@@ -65,7 +67,7 @@ public class FamilyTreeConsole {
         presenter.addPerson(name, "", gender, birthDate);
     }
 
-    private static void findPerson() {
+    private void findPerson() {
         System.out.println("Введите имя:");
         String name = scanner.nextLine();
         Family_Tree.Person person = presenter.findPersonByName(name, "");
@@ -77,22 +79,31 @@ public class FamilyTreeConsole {
         }
     }
 
-    private static void saveTree() {
+    private void saveTree() {
         System.out.println("Введите имя файла:");
         String fileName = scanner.nextLine();
         presenter.saveToFile(fileName);
         System.out.println("Дерево сохранено в файл " + fileName);
     }
 
-    private static void loadTree() {
+    private void loadTree() {
         System.out.println("Введите имя файла:");
         String fileName = scanner.nextLine();
         presenter.loadFromFile(fileName);
         System.out.println("Дерево загружено из файла " + fileName);
     }
 
-    private static void printTree() {
+    private void printTree() {
         System.out.println("Текущее содержимое дерева:");
         System.out.println(presenter.getPeople());
+    }
+
+    public static void main(String[] args) {
+        FamilyTree<Family_Tree.Person> familyTree = new FamilyTree<>();
+        FamilyTreePresenter presenter = new FamilyTreePresenter(familyTree);
+        FamilyTreeConsoleView view = new FamilyTreeConsoleView(presenter);
+        FamilyTreeConsole console = new FamilyTreeConsole(presenter, view);
+
+        console.start();
     }
 }
