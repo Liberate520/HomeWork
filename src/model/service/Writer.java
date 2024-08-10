c073aa5f10ff466df5a52219e761551abf421c2b
tuyana_rinchinova <67235284+rin4inova@users.noreply.github.com>
2024-08-10 23:56:26 +0800

    Delete src/model directory

--- src/model/service/Writer.java
+++
@@ -1,9 +0,0 @@
-package model.service;
-
-import java.io.Serializable;
-
-public interface Writer {
-    void setFilePath(String filePath);
-    void save(Serializable serializable);
-    Object read();
-}
