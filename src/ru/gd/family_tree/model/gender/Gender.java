9971d43598138b532757b32e8a83237c235a903e
tuyana_rinchinova <67235284+rin4inova@users.noreply.github.com>
2024-08-11 00:01:21 +0800

    Delete src directory

--- src/ru/gd/family_tree/model/gender/Gender.java
+++
@@ -1,7 +0,0 @@
-package ru.gd.family_tree.model.gender;
-
-import java.io.Serializable;
-
-public enum Gender implements Serializable {
-    MALE,FEMALE;
-}
--- src/ru/gd/family_tree/view/View.java
+++
@@ -1,6 +0,0 @@
-package ru.gd.family_tree.view;
-
-public interface View {
-    void start();
-    void printAnswer(int answer);
-}
