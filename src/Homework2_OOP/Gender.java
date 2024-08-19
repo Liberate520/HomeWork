4ede5371b6cb0ed75e36d216bfa91c7bffe1d578
Seberestmain <berest.alescha@gmail.com>
2024-08-19 17:35:00 +0300

    Сделана 3ья работа. Сортирует по имени и дате рождения как и написано в задании.

--- src/Homework2_OOP/Gender.java
+++
@@ -1,5 +0,0 @@
-package Homework2_OOP;
-
-public enum Gender {
-    Male, Female;
-}
--- src/Homework2_OOP/Writer.java
+++
@@ -1,8 +0,0 @@
-package Homework2_OOP;
-
-import java.io.Serializable;
-
-public interface Writer {
-    boolean save(Serializable serializable, String filename);
-    Object read(String filename);
-}
