885b888ea43b4aca12afc9ca8f256b91ccf779f7
Castorki <163339014+Castorki@users.noreply.github.com>
2024-08-19 10:32:23 +0300

    Delete Writer directory
    задвоилось

--- Writer/Writer.java
+++
@@ -1,9 +0,0 @@
-package homeWork.Writer;
-
-import homeWork.Tree.FamilyTree;
-import java.io.IOException;
-
-public interface Writer {
-    void writeToFile(FamilyTree tree, String fileName) throws IOException;
-    FamilyTree readFromFile(String fileName) throws IOException, ClassNotFoundException;
-}
