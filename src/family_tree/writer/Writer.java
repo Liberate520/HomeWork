4dc35bd5d3b803a643be9ad55770d43b957ed4cf
nugmaneg <nugmaneg@gmail.com>
2024-08-25 22:10:15 +0300

    add interface

--- src/family_tree/writer/Writer.java
+++
@@ -1,9 +0,0 @@
-package family_tree.writer;
-
-import java.io.Serializable;
-
-public interface Writer {
-    boolean save(Serializable serializable);
-    Object read();
-    void setFilePath(String filePath);
-}
